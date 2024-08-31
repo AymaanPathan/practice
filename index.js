19009a81072d25d60e0f8a9fc19e580b2a94662a
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-01 03:14:18 +0530

    setTimeout

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-// Constructor Function in closure to make scalable
-
-function Outer() {
-  var count = 0;
-  this.incrementCounter = function () {
-    count++;
-    console.log(count);
-  };
-  this.decrementCounter = function () {
-    count--;
-    console.log(count);
-  };
-}
-
-var counter1 = new Outer();
-counter1.incrementCounter();
+// Set Timeout
+console.log("start");
+setTimeout(() => {
+  console.log("Timer");
+});
+console.log("End");
