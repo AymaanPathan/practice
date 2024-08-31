46069db8f19e481e5f39c92d3ff657dbeb17b1e3
Afzal-24 <aymaanpathan5@gmail.com>
2024-08-31 12:03:46 +0530

    closure example

--- index.js
+++ index.js
@@ -1,48 +1,10 @@
-// Function Statements
-function a() {
-  console.log("a called");
+function outer() {
+  var a = 10;
+  function inner() {
+    console.log(a);
+  }
+  return inner;
 }
 
-// function Expression or Function Declaration
-var b = function () {
-  console.log("B called");
-};
-
-// Anonymous Function -> function with no name
-// function(){
-
-// }
-
-// Named Function Expression
-var c = function xyz() {
-  console.log("c Called");
-};
-c();
-// Parameter and Argument
-function d(params) {
-  console.log(params);
-}
-d(1); //Argument
-
-// First Class Function
-// ability to use function as a value is known as first class function
-// Example 1 :
-function e() {
-  return function eChild(params) {};
-}
-
-// Example 2
-function fPara() {
-  console.log("hello from fPara");
-}
-
-function f(fun) {
-  return fun;
-}
-f(fPara);
-
-// Arrow Function
-const arrow = () => {
-  console.log("Hello from Arrow Function");
-};
-arrow();
+const close = outer();
+close();
