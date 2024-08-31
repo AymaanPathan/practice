3fc3669023ac95f9d325722f1a1fc62e89158e9e
Afzal-24 <aymaanpathan5@gmail.com>
2024-08-31 16:35:46 +0530

    encapsulation with closure

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-function outer() {
-  var a = 10;
-  function inner() {
-    console.log(a);
+// Hiding data and encapsulation with closure most imp advantage of closure
+function Counter() {
+  var count = 0;
+  function increment() {
+    count++;
+    console.log(count);
   }
-  return inner;
+  return increment;
 }
 
-const close = outer();
-close();
+// console.log(count) <- it will throw error thats how closure is used in encapsulation
+// and hiding a data
+
+const add1 = Counter();
+add1(); // 1
+add1(); // 2
+add1(); // 3
+
+const add2 = Counter();
+add2(); // 1  coz we are calling a fresh Counter function
