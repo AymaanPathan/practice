3a5beb6f46688ef17e1d2b664de8e727e6a6e65b
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-02 16:42:43 +0530

    filter

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-// Map
+// Filter
 
-//1. double the value of [5,1,3,2,6]
+// 1 find out all odd number from [5,1,3,2,6]
 
 const arr = [5, 1, 3, 2, 6];
+const oddOnes = arr.filter((num) => num % 2);
 
-const doubledArray = arr.map(function (num) {
-  return num * 2;
-});
+console.log(oddOnes);
 
-console.log(doubledArray);
+// when we use num % 2===0 it will filtered out all even number
+// because all numbers should ne divisible by 2 and the remainder
+// should always be 0
 
-//2. Triple the value of [5,1,3,2,6]
+// when we user num%2 it will filtered out all odd number
+// because all number should be divisible by 2 and
+// not remainder will not be 0
+// and in js 0 is a false val so  filter function provides
+// true value only and it will ignore 0 coz its falsy
+// for example 3 % 2 it will give a remainder 1.5
+// for example 2%2 it will give remainder 0 so the filter
+// will ignore it and will not put in array
 
-const arr1 = [5, 1, 3, 2, 6];
+// 2 find out all even number from [5,1,3,2,6]
 
-const TripledArray = arr.map(function (num) {
-  return num * 3;
-});
-console.log(TripledArray);
+const arr2 = [5, 1, 3, 2, 6];
+const evenOnes = arr.filter((num) => num % 2 === 0);
 
-//3. binary of [5,1,3,2,6]
+console.log(evenOnes);
+
+// 2 find out all number which is graeater than 4 [5,1,3,2,6]
 
 const arr3 = [5, 1, 3, 2, 6];
-const binaryArray = arr3.map(function (num) {
-  return num.toString(2);
-});
+const greaterThanFour = arr.filter((num) => num > 4);
 
-console.log(binaryArray);
+console.log(greaterThanFour);
