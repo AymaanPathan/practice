18cce01457af998a468764a0f6e8dc25ca64d8d5
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-02 12:56:54 +0530

    Analyzing setTimeOut

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-// Set Timeout
-console.log("start");
-setTimeout(() => {
+console.log("Start");
+
+function cb() {
   console.log("Timer");
-});
-console.log("End");
+}
+
+setTimeout(cb, 1000);
+
+let start = new Date().getTime();
+let end = new Date().getTime() + 5000;
+let count = 0;
+while (start <= end) {
+  count++;
+  start = new Date().getTime();
+}
+
+console.log(count);
