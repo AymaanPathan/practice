a92f7d8b6058db4363358792b3fe5f8b7143f871
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-02 17:08:31 +0530

    reduce

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-// Filter
+// Reduce
 
-// 1 find out all odd number from [5,1,3,2,6]
+//1. sum of all [5,1,3,2,6]
 
 const arr = [5, 1, 3, 2, 6];
-const oddOnes = arr.filter((num) => num % 2);
+const sum = arr.reduce((acc, curr) => {
+  return acc + curr;
+}, 0);
 
-console.log(oddOnes);
+// console.log(sum);
 
-// when we use num % 2===0 it will filtered out all even number
-// because all numbers should ne divisible by 2 and the remainder
-// should always be 0
+// 0+5 = 5
+// 5+1 =6
+// 6+3 = 9
+// 9+2=11
+// 11+6 = 17 will be output
 
-// when we user num%2 it will filtered out all odd number
-// because all number should be divisible by 2 and
-// not remainder will not be 0
-// and in js 0 is a false val so  filter function provides
-// true value only and it will ignore 0 coz its falsy
-// for example 3 % 2 it will give a remainder 1.5
-// for example 2%2 it will give remainder 0 so the filter
-// will ignore it and will not put in array
+// 2 find max [with for loop]
+function findMax() {
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
 
-// 2 find out all even number from [5,1,3,2,6]
+console.log(findMax());
 
-const arr2 = [5, 1, 3, 2, 6];
-const evenOnes = arr.filter((num) => num % 2 === 0);
+// with reduce
 
-console.log(evenOnes);
+const maxNum = arr.reduce((acc, curr) => {
+  if (acc < curr) {
+    acc = curr;
+  }
+  return acc;
+}, 0);
 
-// 2 find out all number which is graeater than 4 [5,1,3,2,6]
-
-const arr3 = [5, 1, 3, 2, 6];
-const greaterThanFour = arr.filter((num) => num > 4);
-
-console.log(greaterThanFour);
+console.log(maxNum);
