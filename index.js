b9f70d5aba0c792dc6c11200572e4b1ecbafd46d
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-04 17:11:16 +0530

    promise.any

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const promise1 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve("Promise 1 success"), 3000);
+  setTimeout(() => resolve("Promise 1 success"), 2000);
 });
 
 const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve("Promise 2 success"), 2000);
+  setTimeout(() => resolve("Promise 2 success"), 3000);
 });
 
 const promise3 = new Promise((resolve, reject) => {
-  setTimeout(() => reject("Promise 3 success"), 5000);
+  setTimeout(() => reject("Promise 3 success"), 1000);
 });
-Promise.race([promise1, promise2, promise3]).then((data) => console.log(data));
+Promise.any([promise1, promise2, promise3]).then((data) => console.log(data));
