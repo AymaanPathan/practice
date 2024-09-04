d9ab31af893a6e13e86e1bc7ba84fd346b7b7efa
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-04 15:39:42 +0530

    promise.all

--- index.js
+++ index.js
@@ -1,82 +1,13 @@
-// make a promise of createOrder,proceedpayment,show order summary, update wallet
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Promise 1 success"), 3000);
+});
 
-const cart = ["jeans", "t-shirt", "jacket"];
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Promise 2 success"), 1000);
+});
 
-createOrder(cart)
-  .then((data) => {
-    console.log(data);
-    return data;
-  })
-  .then((orderId) => {
-    return proceedPaymet(orderId);
-  })
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Promise 3 success"), 2000);
+});
 
-  .then((cartInfo) => {
-    return orderSummary(cartInfo);
-  })
-  .then((data) => console.log(data))
-  .then(() => {
-    return updateWallet();
-  })
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err))
-  .then(() => {
-    console.log("the api fetching is over");
-  });
-
-function createOrder(cart) {
-  const promise = new Promise((resolve, reject) => {
-    if (cart.length >= 1) {
-      resolve(1234);
-    } else {
-      reject("Please Add Product To Cart before Procceding");
-    }
-  });
-  return promise;
-}
-
-function proceedPaymet(orderId) {
-  const promise = new Promise((resolve, reject) => {
-    if (!validCart(cart)) {
-      reject("Cart is Not Valid");
-    } else {
-      resolve(`The payment of rupees 2000 is successfull id is ${orderId}`);
-    }
-  });
-  return promise;
-}
-
-function orderSummary(cartInfo) {
-  const promise = new Promise((resolve, reject) => {
-    if (cartInfo) {
-      setTimeout(() => {
-        resolve("Generating a report summary");
-      }, 3000);
-    } else {
-      reject("Summary is will be not created");
-    }
-  });
-  return promise;
-}
-
-function updateWallet() {
-  let cash = 2000;
-  const promise = new Promise((resolve, reject) => {
-    if (cash) {
-      setTimeout(() => {
-        resolve(`Current Balance is ${cash - 1340}`);
-      }, 1000);
-    } else {
-      reject("There is some issue with wallet");
-    }
-  });
-  return promise;
-}
-
-function validCart(cart) {
-  if (cart) {
-    return true;
-  } else {
-    false;
-  }
-}
+Promise.all([promise1, promise2, promise3]).then((data) => console.log(data));
