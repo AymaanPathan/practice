0406ae564fdf841de62151bc05ec49abadf10cc2
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-05 16:13:07 +0530

    async await

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("promise resolved");
-  }, 5000);
+    resolve("promise resolved 1");
+  }, 10000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("promise resolved 2");
+  }, 6000);
 });
 
 async function a() {
-  console.log("hello world"); // this will print instantly
-  const data = await p1;
-  console.log("hello"); // this will take 5 second and js engine will waiting for promise
-  console.log(data); // this will also take 5 second prints after hello
+  console.log("hello world");
+  const data1 = await p1;
+
+  console.log("Im First but...");
+  const data2 = await p2;
+
+  console.log(data1);
+  console.log(data2);
 }
 
 a();
