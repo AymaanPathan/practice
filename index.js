1d97832a961b1994b4d25eec4c05a3a080ea4cc2
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-06 00:39:27 +0530

    basic this keyword

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-const p1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject("promise resolved 1");
-  }, 6000);
-});
+"use strict";
 
-const p2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("promise resolved 2");
-  }, 10000);
-});
+const obj = {
+  a: 10,
+  b: function () {
+    console.log(this);
+  },
+};
 
-async function a() {
-  console.log("hello world");
-  const [data1, data2] = await Promise.all([p1, p2]);
-  console.log(data1, data2);
-}
-
-a();
+obj.b();
