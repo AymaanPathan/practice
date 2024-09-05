47803a3a1711e025a4a466dfa3a2884ca8e34bdb
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-05 16:32:30 +0530

    async await promise.all

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("promise resolved 1");
-  }, 10000);
+    reject("promise resolved 1");
+  }, 6000);
 });
 
 const p2 = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve("promise resolved 2");
-  }, 6000);
+  }, 10000);
 });
 
 async function a() {
   console.log("hello world");
-  const data1 = await p1;
-
-  console.log("Im First but...");
-  const data2 = await p2;
-
-  console.log(data1);
-  console.log(data2);
+  const [data1, data2] = await Promise.all([p1, p2]);
+  console.log(data1, data2);
 }
 
 a();
