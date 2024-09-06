5133520489c9fabe8cc485a1e1da60702bc33220
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-06 11:46:27 +0530

    bind method detail

--- index.js
+++ index.js
@@ -18,9 +18,6 @@ let person2 = {
   lastName: "clinton",
 };
 
-//  function borrowing
-
-printFullName.apply(person2, ["india"]); // this is how we can set a value in argument in apply
-
-// the only difference in call and apply is how we pass and argument in call we can
-// pass with comma seperated and in apply witg array list
+//  bind method gives you the copy of that method which can be invoke later
+const print = printFullName.bind(person2, "india");
+print();
