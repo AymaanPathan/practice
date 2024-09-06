e585b1bdb1920942b093af24e3a38da31e2684f8
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-07 01:21:35 +0530

    rotate array

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-// reverse array
-function reverse(arr) {
-  let start = 0;
-  let end = arr.length - 1;
-  while (start <= end) {
+// Rotate
+
+function reverse(arr, start, end) {
+  while (start < end) {
     let temp = arr[start];
     arr[start] = arr[end];
     arr[end] = temp;
-
     start++;
     end--;
   }
 }
 
-const arr = [1, 2, 3, 4, 5];
+function rotate(arr, k) {
+  k = k % arr.length;
+  reverse(arr, 0, arr.length - 1);
+  reverse(arr, 0, k - 1);
+  reverse(arr, k, arr.length - 1);
+}
 
-console.log(reverse(arr));
+const arr = [1, 2, 3, 4, 5];
+const k = 2;
+
+rotate(arr, k);
+
+console.log(arr);
