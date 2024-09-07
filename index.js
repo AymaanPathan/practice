0f19165f2d3762d575b4c5dbf2235fdef253eedd
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-08 01:33:11 +0530

    buy & sell stock

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-function getMinMax(arr) {
-  let max = arr[0];
-  let min = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (max < arr[i]) {
-      max = arr[i];
+var maxProfit = function (prices) {
+  let buy = prices[0]; // we hav to buy in day 1
+  let profit = 0; // in day one profit is 0 cause buy and sell in same day
+  for (let i = 0; i < prices.length; i++) {
+    if (buy > prices[i]) {
+      // if buying price on current day is greator than next then next day price i will buy
+      buy = prices[i];
+    } else {
+      let currentProfit = prices[i] - buy; // else if next day price is high than next day price - today price
+      profit = Math.max(currentProfit, profit);
     }
   }
-  for (let i = 0; i < arr.length; i++) {
-    if (min > arr[i]) {
-      min = arr[i];
-    }
-  }
-  return [min, max];
-}
+  return profit;
+};
 
-const arr = [3, 2, 1, 56, 10000, 167];
-getMinMax(arr);
-console.log(getMinMax(arr));
+const p = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(p));
