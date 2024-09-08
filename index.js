59e1d531924df7546d43fa28c3914986715f4edf
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-09 00:32:08 +0530

    spread example 3

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-let ritikRuns = { name: "Ritik", runs: 0 };
+let ritikRuns = {
+  name: "Ritik",
+  runs: 0,
+  ball: { type: "leather" },
+  time: { day: "Wednesday" },
+};
 let otherRuns = { ...ritikRuns };
-otherRuns.runs = 90;
+otherRuns.ball.time = "tuesday";
 
 console.log(ritikRuns);
 console.log(otherRuns);
