22df4be07a63b67450e29344822682dbed4e5f08
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-08 14:45:17 +0530

    spread operator

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-var maxProfit = function (prices) {
-  let buy = prices[0]; // we hav to buy in day 1
-  let profit = 0; // in day one profit is 0 cause buy and sell in same day
-  for (let i = 0; i < prices.length; i++) {
-    if (buy > prices[i]) {
-      // if buying price on current day is greator than next then next day price i will buy
-      buy = prices[i];
-    } else {
-      let currentProfit = prices[i] - buy; // else if next day price is high than next day price - today price
-      profit = Math.max(currentProfit, profit);
-    }
-  }
-  return profit;
-};
+// Spread Operator
+function addNumber(a, b, c) {
+  return a + b;
+}
 
-const p = [7, 1, 5, 3, 6, 4];
-console.log(maxProfit(p));
+const arr = [2, 3];
+console.log(addNumber(...arr));
