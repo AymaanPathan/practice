16f7810838455792f5f4b7af37038c148729f727
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-08 16:17:49 +0530

    spread example 1

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-// Rest Operator
-const arr = [3, 5, 8, 9, 12, 14];
-const [a, , , ...nums] = arr;
-console.log(a, nums);
+//  Spread operator -> needs iterable like array or string
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+
+// const numbers = [1, 2, 3];
+// console.log(sum(...numbers));
+
+const arr = [1, 2, 3, 4];
+const sum = [...arr].reduce((acc, curr) => {
+  return acc + curr;
+}, 0);
+console.log(sum);
