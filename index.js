d1c103200cefdc146c12992234eeebbe91feb4a3
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-10 22:51:15 +0530

    second-Largest [better]

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-function secondLargest(nums) {
-  let largest = nums[nums.length - 1];
-  let secondLargest = -1;
+// Only works with sorted array
 
-  for (let i = nums.length - 2; i >= 0; i--) {
-    if (nums[i] !== largest) {
+function secondLargest(nums) {
+  let largest = nums[0];
+  let secondLargest = -1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > largest) {
+      largest = nums[i];
+    }
+  }
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] > secondLargest && nums[i] != largest) {
       secondLargest = nums[i];
-      break;
     }
   }
   return secondLargest;
 }
-const arr = [1, 2, 3, 4, 5];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 console.log(secondLargest(arr));
