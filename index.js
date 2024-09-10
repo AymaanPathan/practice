0868023c9bc722af01c297d261a11d8bf7a8901d
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-11 00:34:55 +0530

    second-largest

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-function secondLargest(nums) {
-  let largest = nums[0];
-  let secondLargest = -1;
+function thirdLargest(nums) {
+  if (nums.length < 3) {
+    return "Array needs to have at least 3 numbers";
+  }
+
+  let largest = "";
+  let secondLargest = "";
+  let thirdLargest = "";
+
+  // Find the largest number
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] > largest) {
       largest = nums[i];
     }
   }
+
+  // Find the second largest number
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] > secondLargest && nums[i] !== largest) {
       secondLargest = nums[i];
     }
   }
-  return secondLargest;
-}
 
-function secondSmallest(nums) {
-  let smallest = nums[0];
-  let secondSmallest = Infinity;
-  for (let i = 0; i < nums.length - 1; i++) {
-    if (nums[i] < smallest) {
-      smallest = nums[i];
+  // Find the third largest number
+  for (let i = 0; i < nums.length; i++) {
+    if (
+      nums[i] > thirdLargest &&
+      nums[i] !== largest &&
+      nums[i] !== secondLargest
+    ) {
+      thirdLargest = nums[i];
     }
   }
-  for (let i = 0; i < nums.length - 1; i++) {
-    if (nums[i] < secondSmallest && nums[i] !== smallest) {
-      secondSmallest = nums[i];
-    }
-  }
-  return secondSmallest;
+
+  return thirdLargest;
 }
 
-const arr = [1, 2, 3, 4, 5];
-console.log(secondLargest(arr));
-console.log(secondSmallest(arr));
+const arr = ["aa", "aaa", "aaaa", "a"];
+console.log(thirdLargest(arr));
