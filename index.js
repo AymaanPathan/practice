62d075b34f336a6c0dbb217004766a0280f8db1d
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-12 00:14:38 +0530

    remove duplicate

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-var moveZeroes = function (nums) {
-  let nonZeroIndex = 0;
+var removeDuplicates = function (nums) {
+  let newArr = [];
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      nums[nonZeroIndex] = nums[i];
-      nonZeroIndex++;
+    if (!newArr.includes(nums[i])) {
+      newArr.push(nums[i]);
     }
   }
-  for (let i = nonZeroIndex; i < nums.length; i++) {
-    nums[i] = 0;
-  }
-  return nums;
+  return newArr;
 };
 
-const nums = [0, 1, 0, 3, 12];
-console.log(moveZeroes(nums));
+const arr = [1, 1, 2];
+console.log(removeDuplicates(arr));
