b22e1713885ffca96c41a6395c1e66b14c4981ae
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-11 12:35:48 +0530

    occurence of number

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
-function thirdLargest(nums) {
-  if (nums.length < 3) {
-    return "Array needs to have at least 3 numbers";
-  }
-
-  let largest = "";
-  let secondLargest = "";
-  let thirdLargest = "";
-
-  // Find the largest number
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > largest) {
-      largest = nums[i];
+function occurenceOfNumber(number, arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === number) {
+      count++;
     }
   }
-
-  // Find the second largest number
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > secondLargest && nums[i] !== largest) {
-      secondLargest = nums[i];
-    }
-  }
-
-  // Find the third largest number
-  for (let i = 0; i < nums.length; i++) {
-    if (
-      nums[i] > thirdLargest &&
-      nums[i] !== largest &&
-      nums[i] !== secondLargest
-    ) {
-      thirdLargest = nums[i];
-    }
-  }
-
-  return thirdLargest;
+  return count;
 }
 
-const arr = ["aa", "aaa", "aaaa", "a"];
-console.log(thirdLargest(arr));
+const arr = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 3];
+console.log(occurenceOfNumber(3, arr));
