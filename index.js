e27461969637900449d17e37f3e576272028d51f
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-12 23:08:13 +0530

    missing number

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-var removeDuplicates = function (nums) {
-  let newArr = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (!newArr.includes(nums[i])) {
-      newArr.push(nums[i]);
+var bubbleSort = function (nums) {
+  let n = nums.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (nums[j] > nums[j + 1]) {
+        let temp = nums[j];
+        nums[j] = nums[j + 1];
+        nums[j + 1] = temp;
+      }
     }
   }
-  return newArr;
+  return nums;
 };
 
-const arr = [1, 1, 2];
-console.log(removeDuplicates(arr));
+var missingNumber = function (nums) {
+  bubbleSort(nums);
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== i) {
+      return i;
+    }
+  }
+  return nums.length;
+};
+
+const arr = [9, 6, 4, 2, 3, 5, 7, 0, 1]; // 0,1,2,3,4,5,6,7,9
+console.log("Missing Number:", missingNumber(arr));
