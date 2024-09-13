2a620b46224bbd128b33d371e7aaa155608f886c
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-14 00:41:33 +0530

    move zeroes to end again

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-var singleNumber = function (nums) {
+function moveZeroes(nums) {
+  let nonZeros = 0;
+
   for (let i = 0; i < nums.length; i++) {
-    let count = 0;
-    for (let j = 0; j < nums.length; j++) {
-      if (nums[j] === nums[i]) {
-        count++;
-      }
-      if (count === 1) {
-        return nums;
-      }
+    if (nums[i] > 0) {
+      nums[nonZeros] = nums[i];
+      nonZeros++;
     }
   }
-};
-const arr = [2, 2, 1];
-console.log(singleNumber(arr));
+  for (let j = nonZeros; j < nums.length; j++) {
+    nums[j] = 0;
+  }
+  return nums;
+}
+
+const arr = [0, 1, 0, 3, 12];
+console.log(moveZeroes(arr));
