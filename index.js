0561db4907666f51e067156dadf785d2ab6625a5
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-15 23:04:38 +0530

    higher order function

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-function moveZeroes(nums) {
-  let nonZeros = 0;
-
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > 0) {
-      nums[nonZeros] = nums[i];
-      nonZeros++;
-    }
-  }
-  for (let j = nonZeros; j < nums.length; j++) {
-    nums[j] = 0;
-  }
-  return nums;
+function a(fun) {
+  fun();
 }
 
-const arr = [0, 1, 0, 3, 12];
-console.log(moveZeroes(arr));
+function sayHi() {
+  console.log("hello from function b");
+}
+
+a(sayHi);
