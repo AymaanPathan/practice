6a23290a627ce40374f3743403b42cf2bd51fcae
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-19 01:29:47 +0530

    middlewares node js

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-function a(params) {
-  console.log(this);
-}
-a();
+const express = require("express");
+
+const app = express();
+const port = 3000;
+let userLoggedIn = true;
+
+app.use((req, res, next) => {
+  if (!userLoggedIn) {
+    return res.status(401).json({ message: "Please Login" });
+  }
+  next();
+});
+
+app.use((req, res) => {
+  return res.status(200).json({ message: "Login Success" });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
