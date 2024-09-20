059ed00c5126451e2e210b657bbd7d709c140726
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-20 23:22:05 +0530

    Object.entries [nested]

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const user = {
   name: "aymaan",
   age: 21,
   job: "Software Developer",
+  partner: { mom: "shahin", dad: "abid" },
 };
 
-Object.keys(user).forEach((key) => {
-  console.log(key + ":" + " " + user[key]);
+Object.entries(user).forEach(([key, value]) => {
+  if (typeof value === "object" && value !== null) {
+    Object.entries(value).forEach(([nestKey, nestedVakue]) => {
+      console.log(`${nestKey}`);
+    });
+  }
 });
