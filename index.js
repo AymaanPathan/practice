4af93ff803945e9d56503be8a7f3e5691fb64207
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-20 22:53:30 +0530

    Object.entries

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-const express = require("express");
+const user = {
+  name: "aymaan",
+  age: 21,
+  job: "Software Developer",
+};
 
-const app = express();
-const port = 3000;
-let userLoggedIn = true;
-
-app.use((req, res, next) => {
-  if (!userLoggedIn) {
-    return res.status(401).json({ message: "Please Login" });
-  }
-  next();
-});
-
-app.use((req, res) => {
-  return res.status(200).json({ message: "Login Success" });
-});
-
-app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
-});
+const objArray = Object.entries(user);
+console.log(objArray);
