d3f339eecc2bd8fc0498e7747ee5745f5622c6a7
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-21 11:23:49 +0530

    object deep & shallow copy

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const user = {
   name: "aymaan",
+  partner: { dad: "Abid", mom: "shahin" },
 };
 
-const user2 = user;
-user2.name = "bob";
+// const user2 = { ...user }; // shallow copy
+const user2 = JSON.parse(JSON.stringify(user)); // deep copy
+user2.partner.dad = "bob";
 
 console.log(user);
+console.log(user2);
