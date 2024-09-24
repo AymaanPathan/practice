d4bbd1c4a0e386b8847b785e0f11548da013cabd
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-24 13:33:10 +0530

    leader

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const stringToNumbers = (numbers) => {
-  const numberWords = [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-  ];
-  return numbers.map((num) => {
-    return num
-      .toString()
-      .split("")
-      .map((num) => numberWords[num])
-      .join("-");
-  });
-};
-const arr = [12, 21];
-console.log(stringToNumbers(arr));
+function Reverse(nums) {
+  let start = 0;
+  let end = nums.length - 1;
+  while (start < end) {
+    let temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start++;
+    end--;
+  }
+  return nums;
+}
+function Leaders(nums) {
+  let newArr = [];
+  let maxLeader = nums[nums.length - 1];
+  newArr.push(maxLeader);
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] > maxLeader) {
+      newArr.push(nums[i]);
+      maxLeader = nums[i];
+    }
+  }
+  return Reverse(newArr);
+}
+
+const arr = [16, 17, 4, 3, 5, 2];
+console.log(Leaders(arr));
