b08944c68880ef1bceba85fa40f57cd2cbf81516
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-25 22:52:53 +0530

    order Agnostic BinarySearch

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-function capitalFirst(sentence) {
-  return sentence
-    .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
+function orderAgnosticBinarySearch(nums, target) {
+  // when we dont know if array is decs or asc
+  let start = 0;
+  let end = nums.length - 1;
+  let isAscending;
+  if (nums[start] < nums[end]) {
+    isAscending = true;
+  } else {
+    isAscending = false;
+  }
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+    if (isAscending) {
+      if (nums[mid] < target) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    } else {
+      if (nums[mid] < target) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+  }
+  return -1;
 }
-
-const sentence = "hello world from javascript";
-console.log(capitalFirst(sentence));
+const arr = [6, 5, 4, 3, 2, 1];
+console.log(binarySearch(arr, 2));
