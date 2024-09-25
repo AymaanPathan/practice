bd2851328b521f3913556c259dc9cc6338ca4167
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-25 11:31:37 +0530

    search in 2d array [linear]

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-function searchLinear(nums) {
+function searchLinear(nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = 0; j < nums[i].length; j++) {
-      return nums[j][i];
+      if (nums[i][j] === target) {
+        return true;
+      }
     }
   }
+  return false;
 }
 let arr = [
   [3, 12, 9],
   [5, 2, 89],
   [90, 45, 22],
 ];
-console.log(searchLinear(arr));
+const target = 91;
+console.log(searchLinear(arr, target));
