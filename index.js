e2462a6e3ced39e175ee7fc824394cc20f6d525a
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-25 11:18:28 +0530

    2d array

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-function capitalize(sentence) {
-  return sentence
-    .split(" ")
-    .map((word) => {
-      return word.charAt(0).toUpperCase() + word.slice(1);
-    })
-    .join(" ");
+function searchLinear(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums[i].length; j++) {
+      return nums[j][i];
+    }
+  }
 }
-
-const sentence = "im aymaan and a coding in mindfull place";
-console.log(capitalize(sentence));
+let arr = [
+  [3, 12, 9],
+  [5, 2, 89],
+  [90, 45, 22],
+];
+console.log(searchLinear(arr));
