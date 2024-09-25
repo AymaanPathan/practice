4d0615e3d7962446b8dfbbe9dddf9a61c498001b
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-25 10:21:57 +0530

    capital first

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-function removeDuplicates(nums) {
-  let uniqueIndex = 1;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] !== nums[i - 1]) {
-      nums[uniqueIndex] = nums[i];
-      uniqueIndex++;
-    }
-  }
-  return uniqueIndex;
+function capitalize(sentence) {
+  return sentence
+    .split(" ")
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
 }
-const arr = [1, 2, 3, 3, 4, 5]; //[1,2,3,4,5]
-console.log(removeDuplicates(arr));
+
+const sentence = "im aymaan and a coding in mindfull place";
+console.log(capitalize(sentence));
