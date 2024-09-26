912a0a31ca855919985023e4f2dda6b384d290ad
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-26 11:47:36 +0530

    ceiling of a number

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
-function orderAgnosticBinarySearch(nums, target) {
-  // when we dont know if array is decs or asc
+function ceiling(nums, target) {
   let start = 0;
   let end = nums.length - 1;
-  let isAscending;
-  if (nums[start] < nums[end]) {
-    isAscending = true;
-  } else {
-    isAscending = false;
-  }
   while (start <= end) {
     let mid = Math.floor((start + end) / 2);
     if (nums[mid] === target) {
-      return mid;
+      return nums[mid];
     }
-    if (isAscending) {
-      if (nums[mid] < target) {
-        start = mid + 1;
-      } else {
-        end = mid - 1;
-      }
-    } else {
-      if (nums[mid] < target) {
-        end = mid - 1;
-      } else {
-        start = mid + 1;
-      }
+    if (target > nums[mid]) {
+      start = mid + 1;
+    }
+    if (target < nums[mid]) {
+      end = mid - 1;
     }
   }
-  return -1;
+  return nums[start];
 }
-const arr = [6, 5, 4, 3, 2, 1];
-console.log(binarySearch(arr, 2));
+const arr = [2, 3, 5, 9, 14, 16, 18];
+console.log(ceiling(arr, 17));
