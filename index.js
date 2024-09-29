1f7fabe9280e278810eb56f6004eaa7241793254
Afzal-24 <aymaanpathan5@gmail.com>
2024-09-29 12:00:43 +0530

    order agnostic binary search

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-function ceiling(nums, target) {
+function orderAgnostic(nums, target) {
   let start = 0;
   let end = nums.length - 1;
+  let isAscending;
+  if (nums[start] < nums[end]) {
+    isAscending = true;
+  } else {
+    isAscending = false;
+  }
   while (start <= end) {
     let mid = Math.floor((start + end) / 2);
     if (nums[mid] === target) {
       return mid;
     }
-    if (target > nums[mid]) {
-      start = mid + 1;
+    if (isAscending) {
+      if (target > nums[mid]) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
     } else {
-      end = mid - 1;
+      if (target < nums[mid]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
     }
   }
-  return nums[start];
+  return -1;
 }
 
-const arr = [2, 3, 5, 7, 14, 16, 18];
-console.log(ceiling(arr, 6));
+const arr = [1, 2, 3, 4, 5, 6];
+console.log(orderAgnostic(arr, 5));
