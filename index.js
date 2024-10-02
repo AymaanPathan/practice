5079a567cd5a8fd8f1568a9c4fdc20ebb0427523
Afzal-24 <aymaanpathan5@gmail.com>
2024-10-02 12:00:14 +0530

    substring basics

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 const me = "Aymaan";
-const sliceStr = me.slice(0, -1);
-console.log(sliceStr);
-
-// A y m a a n
-// 0 1 2 3 4 5          [positions in Positive]
-// -6 -5 -4 -3 -2 -1   [positions in negative]
+const substring = me.substring(-5, 1); // -5 = 0
+console.log(substring);
