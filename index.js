2940d89aec92d8fd53dc111a2871aa67545e1ae0
Aymaan <aymaanpathan5@gmail.com>
2024-10-03 00:44:47 +0530

    Anagram basics

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-function palindrome(str) {
-  let start = 0;
-  let end = str.length - 1;
-  while (start < end) {
-    if (str.charAt(start) !== str.charAt(end)) {
+function isAnagram(s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+  let count = {};
+  for (let i = 0; i < s.length; i++) {
+    let letter = s[i];
+    if (!count[letter]) {
+      count[letter] = 1;
+    } else {
+      count[letter]++;
+    }
+  }
+  for (let j = 0; j < t.length; j++) {
+    let letter = t[j];
+    if (!count[letter]) {
       return false;
     }
-    start++;
-    end--;
+    count[letter]--;
   }
   return true;
 }
-const str = "A man, a plan, a canal: Panama";
-console.log(palindrome(str));
+const s = "hello";
+const l = "hello";
+console.log(isAnagram(s, l));
