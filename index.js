d48c7d1c3b86f6f6647e129e3f09b4a080a98188
Aymaan <aymaanpathan5@gmail.com>
2024-10-02 13:32:55 +0530

    startsWith and endsWith

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const me = "Aymaan";
-console.log(me.padEnd(7, "*")); //4 stars because me is 6 letters already init
-// 7-6 = 1 so 1 start
+console.log(me.startsWith("A"));
+console.log(me.endsWith("n"));
