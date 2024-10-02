99ba4cf476982e6b28a1a6ee475c3bf3e52d47a9
Afzal-24 <aymaanpathan5@gmail.com>
2024-10-02 11:13:59 +0530

    string charAt and indexOf

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-function searchinMatrix(nums, target) {
-  for (let row = 0; row < nums.length; row++) {
-    for (let col = 0; col < nums[row].length; col++) {
-      if (target === nums[row][col]) {
-        return [row, col];
-      }
-    }
-  }
-  return [-1, -1];
-}
-const arr = [[1, 2, 3, 4, 5, 6]];
-console.log(searchinMatrix(arr, 3));
+const me = "Aymaan";
+console.log(me.charAt(2)); //m
+console.log(me.indexOf("m")); //2
