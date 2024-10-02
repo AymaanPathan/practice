14fa0e73d5ae5de14569a90fa705ef1fd8c81e97
Aymaan <aymaanpathan5@gmail.com>
2024-10-02 12:36:11 +0530

    replace-All

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-const me = "Aymaan";
-console.log(me.includes("pathan"));
+const lan = "Java is the best langauge and Java is used for frontend";
+const newstr = lan.replaceAll("Java", "javascript");
+console.log(newstr);
