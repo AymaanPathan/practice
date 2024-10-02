8774f2159722b8a0dd25129f9c572daf0fe05148
Aymaan <aymaanpathan5@gmail.com>
2024-10-02 13:31:47 +0530

    padEnd with formula

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 const me = "Aymaan";
-console.log(me.padEnd(10, "*")); //4 stars because me is 6 letters already init
+console.log(me.padEnd(7, "*")); //4 stars because me is 6 letters already init
+// 7-6 = 1 so 1 start
