445ebd42d0cfa0f56e8f718540c7ad4237579737
Aymaan <aymaanpathan5@gmail.com>
2024-10-08 20:10:55 +0530

    first unique char

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-function countOccurence(nums) {
+var firstUniqChar = function (s) {
   let map = {};
-  for (let i = 0; i < nums.length; i++) {
-    let number = nums[i];
-    if (!map[number]) {
-      map[number] = 1;
+  for (let i = 0; i < s.length; i++) {
+    let letter = s[i];
+    if (!map[letter]) {
+      map[letter] = 1;
     } else {
-      map[number]++;
+      map[letter]++;
     }
   }
-  return map;
-}
-const arr = [1, 2, 3, 4, 1, 2, 3, 3];
-console.log(countOccurence(arr));
+  for (let i = 0; i < s.length; i++) {
+    let letter = s[i];
+    if (map[letter] == 1) {
+      return i;
+    }
+  }
+  return -1;
+};
+const s = "leetcode";
+console.log(firstUniqChar(s));
