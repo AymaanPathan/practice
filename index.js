06e07410c4e0fe19fdacf33379cb0ff95c3ff7a2
Aymaan <aymaanpathan5@gmail.com>
2024-10-08 20:38:13 +0530

    reverse word v2

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-var firstUniqChar = function (s) {
-  let map = {};
-  for (let i = 0; i < s.length; i++) {
-    let letter = s[i];
-    if (!map[letter]) {
-      map[letter] = 1;
-    } else {
-      map[letter]++;
-    }
+var reverseWords = function (s) {
+  let sArray = s.trim().split(/\s+/);
+  let start = 0;
+  let end = sArray.length - 1;
+
+  while (start < end) {
+    let temp = sArray[start];
+    sArray[start] = sArray[end];
+    sArray[end] = temp;
+    start++;
+    end--;
   }
-  for (let i = 0; i < s.length; i++) {
-    let letter = s[i];
-    if (map[letter] == 1) {
-      return i;
-    }
-  }
-  return -1;
+  return sArray.join(" ");
 };
-const s = "leetcode";
-console.log(firstUniqChar(s));
+const s = "a good   example";
+console.log(reverseWords(s));
