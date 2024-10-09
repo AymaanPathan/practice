49194c2cf5c1e97c25526499abc6a84b6311dc06
Aymaan <aymaanpathan5@gmail.com>
2024-10-09 23:13:23 +0530

    rotate string

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var largestOddNumber = function (num) {
-  for (let i = num.length - 1; i >= 0; i--) {
-    if (Number(num[i] % 2 !== 0)) {
-      return num.slice(0, i + 1);
+var rotateString = function (s, goal) {
+  if (s.length !== goal.length) return false;
+
+  for (let i = 0; i < s.length; i++) {
+    let rotateStr = "";
+    for (let j = i; j < s.length; j++) {
+      rotateStr += s[j];
+    }
+    for (let k = 0; k < i; k++) {
+      rotateStr += s[k];
+    }
+    if (rotateStr === goal) {
+      return true;
     }
   }
-  return "";
+  return false;
 };
-const num = "1234";
-console.log(largestOddNumber(num));
+const s = "abcde";
+const goal = "cdeab";
+console.log(rotateString(s, goal));
