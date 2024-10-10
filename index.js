a16893ab703bd0e61a9eccd9a63c169871515de2
Aymaan <aymaanpathan5@gmail.com>
2024-10-10 23:10:43 +0530

    reverse words

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-var romanToInt = function (s) {
-  const map = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
-  };
-  let total = 0;
-
-  for (let i = 0; i < s.length; i++) {
-    if (map[s[i]] >= map[s[i + 1]] || i === s.length - 1) {
-      total = total + map[s[i]];
-    } else {
-      total = total - map[s[i]];
-    }
-  }
-  return total;
+var reverseWords = function (s) {
+  return s
+    .split(" ")
+    .map((word) => {
+      let str = "";
+      for (let i = word.length - 1; i >= 0; i--) {
+        str += word[i];
+      }
+      return str;
+    })
+    .join(" ");
 };
-const s = "III";
-console.log(romanToInt(s));
+const s = "Let's take LeetCode contest";
+console.log(reverseWords(s));
