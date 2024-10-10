8daf52b2afe8605506d159f077af71ea73c08a26
Aymaan <aymaanpathan5@gmail.com>
2024-10-10 11:58:44 +0530

    anagram revise

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-var longestCommonPrefix = function (strs) {
-  strs.sort();
-  let prefixStr = "";
-  let start = strs[0];
-  let end = strs[strs.length - 1];
-  for (let i = 0; i < strs.length; i++) {
-    if (start[i] === end[i]) {
-      prefixStr += start[i];
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  let map = {};
+  for (let i = 0; i < s.length; i++) {
+    let letter = s[i];
+    if (!map[letter]) {
+      map[letter] = 1;
+    } else {
+      map[letter]++;
     }
   }
-  return prefixStr;
+  for (let i = 0; i < t.length; i++) {
+    let letter = t[i];
+    if (!map[letter]) {
+      return false;
+    }
+    map[letter]--;
+  }
+  return true;
 };
-const strs = ["flower", "flow", "flight"];
-console.log(longestCommonPrefix(strs));
+const s = "anagram";
+const t = "nagaram";
+console.log(isAnagram(s, t));
