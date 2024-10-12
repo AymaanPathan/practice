6af0894331e826fb09da966db7f0dd431c5a5298
Aymaan <aymaanpathan5@gmail.com>
2024-10-12 13:34:54 +0530

    camel casing question

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-var strStr = function (haystack, needle) {
-  if (haystack.length < needle.length) return -1;
-  for (let i = 0; i <= haystack.length - needle.length; i++) {
-    if (haystack.substr(i, i + needle.length) === needle) {
-      return i;
+function capitalEveryOtherLetter(str) {
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    let word = str[i];
+    if (i % 2 === 0) {
+      newStr += word.toUpperCase();
+    } else {
+      newStr += word.toLowerCase();
     }
   }
-  return -1;
-};
-
-const haystack = "abc";
-const needle = "c";
-console.log(strStr(haystack, needle));
+  return newStr;
+}
+const str = "HELLO";
+console.log(capitalEveryOtherLetter(str));
+// hello => HeLlO
