244609e546dc155b90544aa318fec1d00da1fa70
Aymaan <aymaanpathan5@gmail.com>
2024-10-17 22:14:38 +0530

    findTheDifference [389]

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-var lengthOfLastWord = function (s) {
-  let str = "";
-  s.trim()
-    .split(" ")
-    .slice(-1)
-    .map((word) => {
-      str += word;
-    });
-  return str.length;
+var findTheDifference = function (s, t) {
+  let map = {};
+  for (let i = 0; i < s.length; i++) {
+    let num = s[i];
+    if (!map[num]) {
+      map[num] = 1;
+    } else {
+      map[num]++;
+    }
+  }
+  for (let j = 0; j < t.length; j++) {
+    let tNum = t[j];
+    if (!map[tNum]) {
+      return tNum;
+    }
+  }
+  for (let j = 0; j < t.length; j++) {
+    let tNum = t[j];
+    if (!map[tNum]) {
+      return tNum;
+    } else {
+      map[tNum]--;
+      if (map[tNum] === -1) {
+        return map;
+      }
+    }
+  }
+  return " ";
 };
-
-const s = "luffy is still joyboy";
-console.log(lengthOfLastWord(s));
+const s = "aa";
+const t = "a";
+console.log(findTheDifference(s, t));
