d064cc3417f923ef678baf05eb853752dc0b9595
Aymaan <aymaanpathan5@gmail.com>
2024-10-18 22:41:21 +0530

    searchInsert [35]

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-function findDuplicate(nums) {
-  let freq = {};
-  let noDuplcates = [];
-  for (let i = 0; i < nums.length; i++) {
-    let num = nums[i];
-    if (!freq[num]) {
-      freq[num] = 1;
-      noDuplcates.push(num);
+var searchInsert = function (nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+  while (start <= end) {
+    let mid = Math.floor(start + end / 1);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      start = mid + 1;
     } else {
-      freq[num]++;
+      end = mid - 1;
     }
   }
-  return noDuplcates;
-}
-const nums = [1, 2, 3, 4, 4, 5, 6, 7, 8, 3, 5, 8, 1, 1];
+  return end + 1;
+};
 
-console.log(findDuplicate(nums));
+const arr = [1, 3, 5, 6];
+console.log(searchInsert(arr, 7));
