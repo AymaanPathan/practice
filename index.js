657112d398e0dbc279f471bff9d27bdd75900410
Aymaan <aymaanpathan5@gmail.com>
2024-10-18 08:55:50 +0530

    duplicates number in array length

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 function findDuplicate(nums) {
   let freq = {};
-  let duplicatesArr = [];
+  let noDuplcates = [];
   for (let i = 0; i < nums.length; i++) {
     let num = nums[i];
     if (!freq[num]) {
       freq[num] = 1;
+      noDuplcates.push(num);
     } else {
       freq[num]++;
     }
-    if (freq[num] === 2) {
-      duplicatesArr.push(num);
-    }
   }
-  return duplicatesArr;
+  return noDuplcates;
 }
 const nums = [1, 2, 3, 4, 4, 5, 6, 7, 8, 3, 5, 8, 1, 1];
 
