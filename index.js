1dc5fb2c76301a5a25ef287246fd09165dd39834
Aymaan <aymaanpathan5@gmail.com>
2024-10-21 22:39:00 +0530

    closure

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-var intersection = function (nums1, nums2) {
-  let arr = [];
-  const freq = {};
-  for (let i = 0; i < num1.length; i++) {
-    let num = num1[i];
-    if (!freq[num]) {
-      freq[num] = 1;
-    } else {
-      freq[num]++;
-    }
+function outer() {
+  var a = 10;
+  function inner() {
+    console.log(a);
   }
-  for (let i = 0; i < num2.length; i++) {
-    let num2 = nums2[i];
-    if (freq[num2]) {
-      if (!arr.includes(num2)) {
-        arr.push(num2);
-      }
-      freq[num2]--;
-    }
-  }
-  return arr;
-};
+  return inner;
+}
 
-const num1 = [1, 2, 2, 1];
-const num2 = [2, 2];
-
-console.log(intersection(num1, num2));
+const abc = outer();
+abc();
