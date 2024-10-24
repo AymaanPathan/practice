e04d3e87ef9e7ba3a533c1eb16907732d6371a58
Aymaan <aymaanpathan5@gmail.com>
2024-10-24 23:49:15 +0530

    linked list [pop]

--- Linked-List/index.js
+++ Linked-List/index.js
@@ -14,19 +14,42 @@ class LinkedList {
   }
 
   push(val) {
-    let addNode = new Node(val);
+    let newNode = new Node(val);
     if (!this.head) {
-      this.head = addNode;
-      this.tail = addNode;
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-      this.tail.next = addNode;
-      this.tail = addNode;
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
     this.length++;
-    return addNode;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let pre = this.head;
+    let temp = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return temp;
   }
 }
 
-const newLinkedList = new LinkedList(4);
-newLinkedList.push(5);
-console.log(newLinkedList);
+let linkedList = new LinkedList(1);
+linkedList.push(2);
+linkedList.push(3);
+linkedList.push(4);
+
+console.log(linkedList.pop());
