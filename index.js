1ddb81c266ef9fbb945f289b52561fd8324a29bc
Aymaan <aymaanpathan5@gmail.com>
2024-11-01 14:24:53 +0530

    LL reverse leetcode 206

--- index.js
+++ index.js
@@ -1,83 +1,11 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
+var reverseList = function (head) {
+  let current = head;
+  let prev = null;
+  while (current) {
+    let next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
   }
-}
-
-class LinkedList {
-  constructor(val) {
-    let newNode = new Node(val);
-    this.head = newNode;
-    this.tail = newNode;
-    this.length = 1;
-  }
-  push(val) {
-    let newNode = new Node(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-    this.length++;
-    return this;
-  }
-  pop() {
-    if (!this.head) {
-      //if no node in linked list
-      return undefined;
-    }
-    let temp = this.head;
-    let pre = this.head;
-    while (temp.next) {
-      pre = temp;
-      temp = temp.next;
-    }
-    this.tail = pre;
-    this.tail.next = null;
-    this.length--;
-    if (this.length === 0) {
-      //if only one node in linkedlist
-      this.head = null;
-      this.tail = null;
-    }
-    return temp;
-  }
-  shift() {
-    if (!this.head) {
-      // if no node in linkedlist
-      return undefined;
-    }
-    let temp = this.head;
-    this.head = this.head.next;
-    temp.next = null;
-    this.length--;
-    if (this.length == 0) {
-      //if one node in linked list
-      this.tail = null;
-    }
-    return temp;
-  }
-
-  Reverse() {
-    let temp = this.head;
-    this.head = this.tail;
-    this.tail = temp;
-    let next = temp.next;
-    let prev = null;
-    for (let i = 0; i < this.length; i++) {
-      next = temp.next;
-      temp.next = prev;
-      prev = temp;
-      temp = next;
-    }
-    return this;
-  }
-}
-const linkedList = new LinkedList(4);
-linkedList.push(1);
-linkedList.push(3);
-
-console.log(linkedList.Reverse());
+  return prev;
+};
