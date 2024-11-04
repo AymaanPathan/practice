0f269485d283597fdc9b198b21a517ff3a33936c
Aymaan <aymaanpathan5@gmail.com>
2024-11-04 12:32:58 +0530

    234. Palindrome Linked List

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-var deleteNode = function (node) {
-  node.val = node.next.val;
-  node.next = node.next.next;
+var isPalindrome = function (head) {
+  let arr = [];
+  let current = head;
+  while (current) {
+    arr.push(current.val);
+    current = current.next;
+  }
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    if (arr[start] !== arr[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+  return true;
 };
