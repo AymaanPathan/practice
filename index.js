431dbf03cf2312628c05ed4456e45315521c88a6
Aymaan <aymaanpathan5@gmail.com>
2024-11-04 12:07:24 +0530

    237. Delete Node in a Linked List

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-var removeElements = function (head, val) {
-  while (head && head.val === val) {
-    head = head.next;
-  }
-  let current = head;
-  let prev = head;
-
-  while (current) {
-    if (current.val === val) {
-      prev.next = current.next;
-    } else {
-      prev = current;
-    }
-    current = current.next;
-  }
-  return head;
+var deleteNode = function (node) {
+  node.val = node.next.val;
+  node.next = node.next.next;
 };
