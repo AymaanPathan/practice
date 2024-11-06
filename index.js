2d69f4db8241387dd5e380cde048968a4fd8d6d6
Aymaan <aymaanpathan5@gmail.com>
2024-11-06 17:42:34 +0530

    21. Merge Two Sorted Lists

--- index.js
+++ index.js
@@ -1,19 +1,59 @@
-var findDuplicate = function (nums) {
-  let map = {};
-  let ans;
-  for (let i = 0; i < nums.length; i++) {
-    let num = nums[i];
-    if (!map[num]) {
-      map[num] = 1;
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+class LinkedList {
+  constructor(val) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  Push(val) {
+    let newNode = new ListNode(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-      map[num]++;
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    if (map[num] > 1) {
-      ans = num;
+    this.length++;
+    return this;
+  }
+}
+
+var mergeTwoLists = function (list1, list2) {
+  let current1 = list1;
+  let current2 = list2;
+  const newLinkedList = new LinkedList();
+
+  while (current1 && current2) {
+    if (current1.val <= current2.val) {
+      newLinkedList.Push(current1.val);
+      current1 = current1.next;
+    } else {
+      newLinkedList.Push(current2.val);
+      current2 = current2.next;
     }
   }
-  return ans;
-};
+  while (current1) {
+    newLinkedList.Push(current1.val);
+    current1 = current1.next;
+  }
 
-const arr = [1, 3, 4, 2, 2];
-console.log(findDuplicate(arr));
+  while (current2) {
+    newLinkedList.Push(current2.val);
+    current2 = current2.next;
+  }
+  return newLinkedList.head;
+};
