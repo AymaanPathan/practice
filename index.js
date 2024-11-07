b445e994eddb5a22259c9658430160877f92cf01
Aymaan <aymaanpathan5@gmail.com>
2024-11-07 17:56:16 +0530

    83. Remove Duplicates from Sorted List

--- index.js
+++ index.js
@@ -6,54 +6,18 @@
  * }
  */
 /**
- * @param {ListNode} list1
- * @param {ListNode} list2
+ * @param {ListNode} head
  * @return {ListNode}
  */
+var deleteDuplicates = function(head) {
+    let current = head;
 
-class LinkedList {
-  constructor(val) {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-
-  Push(val) {
-    let newNode = new ListNode(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
+    while(current && current.next){
+        if(current.val ===current.next.val){
+            current.next = current.next.next
+        } else{
+        current = current.next
+        }
     }
-    this.length++;
-    return this;
-  }
-}
-
-var mergeTwoLists = function (list1, list2) {
-  let current1 = list1;
-  let current2 = list2;
-  const newLinkedList = new LinkedList();
-
-  while (current1 && current2) {
-    if (current1.val <= current2.val) {
-      newLinkedList.Push(current1.val);
-      current1 = current1.next;
-    } else {
-      newLinkedList.Push(current2.val);
-      current2 = current2.next;
-    }
-  }
-  while (current1) {
-    newLinkedList.Push(current1.val);
-    current1 = current1.next;
-  }
-
-  while (current2) {
-    newLinkedList.Push(current2.val);
-    current2 = current2.next;
-  }
-  return newLinkedList.head;
+    return head;
 };
