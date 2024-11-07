4a36dbf9d71e402d9a4a82c15c390bc3ee90b594
Aymaan <aymaanpathan5@gmail.com>
2024-11-07 18:38:01 +0530

    21. Merge Two Sorted Lists

--- index.js
+++ index.js
@@ -6,18 +6,61 @@
  * }
  */
 /**
- * @param {ListNode} head
+ * @param {ListNode} list1
+ * @param {ListNode} list2
  * @return {ListNode}
  */
-var deleteDuplicates = function(head) {
-    let current = head;
 
-    while(current && current.next){
-        if(current.val ===current.next.val){
-            current.next = current.next.next
-        } else{
-        current = current.next
-        }
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    return head;
+    this.length++;
+    return this;
+  }
+}
+
+var mergeTwoLists = function (list1, list2) {
+  let current1 = list1;
+  let current2 = list2;
+  let ans = new LinkedList();
+
+  while (current1 && current2) {
+    if (current1.val <= current2.val) {
+      ans.push(current1.val);
+      current1 = current1.next;
+    } else {
+      ans.push(current2.val);
+      current2 = current2.next;
+    }
+  }
+
+  while (current1) {
+    ans.push(current1.val);
+    current1 = current1.next;
+  }
+
+  while (current2) {
+    ans.push(current2.val);
+    current2 = current2.next;
+  }
+  return ans.head;
 };
