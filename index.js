4dff1323b785bf616bd4e81cc31614a8bf91bc2d
Aymaan <aymaanpathan5@gmail.com>
2024-11-30 11:16:32 +0530

    removeDuplicates

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-var moveZeroes = function (nums) {
-  let nonZero = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      nums[nonZero] = nums[i];
-      nonZero++;
+var removeDuplicates = function (nums) {
+  let unique = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[unique] = nums[i];
+      unique++;
     }
   }
-  for (let i = nonZero; i < nums.length; i++) {
-    nums[i] = 0;
-  }
-  return nums;
+  return unique;
 };
-const nums = [0, 1, 0, 3, 12];
-console.log(moveZeroes(nums));
+const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+console.log(removeDuplicates(nums));
