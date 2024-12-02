037b61cddd348b99eafe66d093e2ebb4b66971ce
Aymaan <aymaanpathan5@gmail.com>
2024-12-02 13:19:52 +0530

    bubble sort revision

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-function insertionSort(nums) {
-  for (let i = 0; i < nums.length - 1; i++) {
-    for (let j = i + 1; j > 0; j--) {
-      if (nums[j] < nums[j - 1]) {
-        let temp = nums[j];
-        nums[j] = nums[j - 1];
-        nums[j - 1] = temp;
-      } else {
-        break;
+function swap(nums, first, second) {
+  let temp = nums[first];
+  nums[first] = nums[second];
+  nums[second] = temp;
+}
+
+function bubbleSort(nums) {
+  for (let i = nums.length; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        swap(nums, j, j + 1);
       }
     }
   }
   return nums;
 }
-
 const nums = [4, 6, 2, 5, 3, 1];
-console.log(insertionSort(nums));
+console.log(bubbleSort(nums));
