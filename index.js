5a34de7b8cfdde0cee8f3f1a114e1b52072c344f
Aymaan <aymaanpathan5@gmail.com>
2024-12-02 12:34:47 +0530

    insertionSort

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var nextGreatestLetter = function (letters, target) {
-  let start = 0;
-  let end = letters.length - 1;
-  while (start <= end) {
-    let mid = Math.floor((start + end) / 2);
-    if (target >= letters[mid]) {
-      start = mid + 1;
-    } else {
-      end = mid - 1;
+function insertionSort(nums) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j > 0; j--) {
+      if (nums[j] < nums[j - 1]) {
+        let temp = nums[j];
+        nums[j] = nums[j - 1];
+        nums[j - 1] = temp;
+      } else {
+        break;
+      }
     }
   }
-  return letters[start % letters.length];
-};
+  return nums;
+}
+
+const nums = [4, 6, 2, 5, 3, 1];
+console.log(insertionSort(nums));
