9667d88cee643bf978ff7ad59f6e1ff212bcc313
Aymaan <aymaanpathan5@gmail.com>
2024-12-03 11:44:21 +0530

    509. Fibonacci Number

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-function funcOne() {
-  funcTwo();
-  console.log("One");
-}
-
-function funcTwo() {
-  funcThree();
-  console.log("Two");
-}
-function funcThree() {
-  console.log("Three");
-}
-
-funcOne();
+var fib = function (n) {
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  return fib(n - 1) + fib(n - 2);
+};
+console.log(fib(2));
