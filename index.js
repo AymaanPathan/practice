2507fe524adb339285703eebbf0fe95f4e77f562
Aymaan <aymaanpathan5@gmail.com>
2024-12-03 12:01:35 +0530

    binary search in recursion

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var fib = function (n) {
-  if (n == 0) return 0;
-  if (n == 1) return 1;
-  return fib(n - 1) + fib(n - 2);
-};
-console.log(fib(2));
+function binarySearch(nums, target, start, end) {
+  if (start > end) {
+    return -1;
+  }
+  let mid = Math.floor((start + end) / 2);
+  if (nums[mid] === target) {
+    return mid;
+  }
+  if (nums[mid] < target) {
+    return binarySearch(nums, target, mid + 1, end);
+  } else {
+    return binarySearch(nums, target, start, mid - 1);
+  }
+}
+
+const arr = [4, 3, 5, 7, 1, 9, 8];
+console.log(binarySearch(arr, 7, 0, arr.length - 1));
