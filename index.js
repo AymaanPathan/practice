ab9144c49d7e2e818d49a4b6870656751d05dfdb
Aymaan <aymaanpathan5@gmail.com>
2024-12-04 13:55:14 +0530

    451. Sort Characters By Frequency

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-function printToN(n) {
-  if (n === 0) return 0;
-  console.log(n);
-  return printToN(n - 1);
-}
-printToN(2);
+var frequencySort = function (s) {
+  const freq = {};
+  let str = "";
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (!freq[char]) {
+      freq[char] = 1;
+    } else {
+      freq[char]++;
+    }
+  }
+
+  const freqArray = Object.entries(freq);
+  freqArray.sort((a, b) => b[1] - a[1]);
+
+  for (let [char, count] of freqArray) {
+    for (let i = 0; i < count; i++) {
+      str += char;
+    }
+  }
+  return str;
+};
+const s = "Aabb";
+console.log(frequencySort(s));
