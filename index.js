b7fdc6ccffd1f684004dba59b9f3dcbce11f0068
Aymaan <aymaanpathan5@gmail.com>
2024-12-04 22:19:04 +0530

    cyclic sort

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-var plusOne = function (digits) {
-  const Array = [];
+function swap(nums, start, end) {
+  let temp = nums[start];
+  nums[start] = nums[end];
+  nums[end] = temp;
+}
 
-  let digitAns = "";
-  for (let i = 0; i < digits.length; i++) {
-    let digitStr = digits[i].toString();
-    digitAns += digitStr;
+function cyclicSort(nums) {
+  let index = 0;
+  while (index < nums.length) {
+    let correct = nums[index] - 1;
+    if (nums[index] != nums[correct]) {
+      swap(nums, index, correct);
+    } else {
+      index++;
+    }
   }
+  return nums;
+}
 
-  let ans = [BigInt(digitAns) + BigInt(1)];
-  let strans = ans.toString();
-  let sArray = strans.split("");
-  for (let i = 0; i < sArray.length; i++) {
-    Array.push(Number(sArray[i]));
-  }
-  return Array;
-};
-const digits = [9];
-console.log(plusOne(digits));
+const arr = [3, 5, 2, 1, 4];
+console.log(cyclicSort(arr));
