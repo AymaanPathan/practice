c066da721a943de9ba8e61cf48168ad38adbad22
Aymaan <aymaanpathan5@gmail.com>
2024-12-04 23:29:43 +0530

    448. Find All Numbers Disappeared in an Array

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ function swap(nums, start, end) {
   nums[start] = nums[end];
   nums[end] = temp;
 }
-
-var missingNumber = function (nums) {
+var findDisappearedNumbers = function (nums) {
+  let ans = [];
   let i = 0;
   while (i < nums.length) {
-    let correct = nums[i];
-    if (nums[i] !== nums[correct] && nums[i] < nums.length) {
-      swap(nums, correct, i);
+    let correct = nums[i] - 1;
+    if (nums[i] !== nums[correct] && nums[i] !== nums[correct]) {
+      swap(nums, i, correct);
     } else {
       i++;
     }
   }
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== i) {
-      return i;
+    if (nums[i] !== i + 1) {
+      ans.push(i + 1);
     }
   }
-  return nums.length;
+  return ans;
 };
-const arr = [0, 1];
-console.log(missingNumber(arr));
+
+const arr = [4, 3, 2, 7, 8, 2, 3, 1];
+console.log(findDisappearedNumbers(arr));
