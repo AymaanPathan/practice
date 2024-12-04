034b238c9ab5ab41cdde83c972333946fd6c3d34
Aymaan <aymaanpathan5@gmail.com>
2024-12-04 23:35:10 +0530

    442. Find All Duplicates in an Array

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ function swap(nums, start, end) {
   nums[start] = nums[end];
   nums[end] = temp;
 }
-var findDisappearedNumbers = function (nums) {
-  let ans = [];
+var findDuplicates = function (nums) {
   let i = 0;
+  let ans = [];
   while (i < nums.length) {
     let correct = nums[i] - 1;
-    if (nums[i] !== nums[correct] && nums[i] !== nums[correct]) {
-      swap(nums, i, correct);
+    if (nums[i] !== nums[correct]) {
+      swap(nums, correct, i);
     } else {
       i++;
     }
   }
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== i + 1) {
-      ans.push(i + 1);
+      ans.push(nums[i]);
     }
   }
   return ans;
 };
 
-const arr = [4, 3, 2, 7, 8, 2, 3, 1];
-console.log(findDisappearedNumbers(arr));
+const arr = [1];
+console.log(findDuplicates(arr));
