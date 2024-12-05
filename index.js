06fd1a26f13e31be8cc476c5e36c044f5cdc4f26
Aymaan <aymaanpathan5@gmail.com>
2024-12-05 11:00:41 +0530

    recursion factorial

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
-function swap(nums, start, end) {
-  let temp = nums[start];
-  nums[start] = nums[end];
-  nums[end] = temp;
+function fact(n) {
+  if (n <= 1) return 1;
+  return n * fact(n - 1);
 }
-var firstMissingPositive = function (nums) {
-  let i = 0;
-  while (i < nums.length) {
-    let correct = nums[i] - 1;
-    if (nums[i] > 0 && nums[i] !== nums[correct] && nums[i] <= nums.length) {
-      swap(nums, correct, i);
-    } else {
-      i++;
-    }
-  }
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== i + 1) {
-      return i + 1;
-    }
-  }
-  return nums.length + 1;
-};
+console.log(fact(5));
