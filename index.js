131bf0898ccf69e6274d5980d443a998344b9efa
Aymaan <aymaanpathan5@gmail.com>
2024-12-06 15:55:50 +0530

    2828. Check if a String Is an Acronym of Words

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-var validMountainArray = function (arr) {
-  if (arr.length < 3) {
-    return false;
+var isAcronym = function (words, s) {
+  let str = "";
+  for (let i = 0; i < words.length; i++) {
+    str += words[i].charAt(0);
   }
-
-  let i = 0;
-
-  while (i < arr.length - 1 && arr[i] < arr[i + 1]) {
-    //check increasing
-    i++;
-  }
-
-  if (i === 0 || i === arr.length - 1) {
-    // if i is check increasing and reach to end this is will be not valid
-    return false;
-  }
-
-  while (i < arr.length - 1 && arr[i] > arr[i + 1]) {
-    // otherwise check in decreasing part
-    i++;
-  }
-  return i == arr.length - 1;
+  return str === s;
 };
-const arr = [0, 3, 2, 1];
-console.log(validMountainArray(arr));
+const words = ["alice", "bob", "charlie"];
+const s = "abc";
+console.log(isAcronym(words, s));
