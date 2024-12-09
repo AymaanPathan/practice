fcf190aade474b2985d9a23bdd39ad3f69e76cc7
Aymaan <aymaanpathan5@gmail.com>
2024-12-09 13:09:16 +0530

    merge sort

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-var findMin = function (nums) {
-  let min = Infinity;
-  for (let i = 0; i < nums.length; i++) {
-    if (min >= nums[i]) {
-      min = nums[i];
-    }
+function mergeSort(nums) {
+  if (nums.length === 1) {
+    return nums;
   }
-  return min;
-};
-const nums = [11, 13, 15, 17];
-console.log(findMin(nums));
+  let mid = Math.floor(nums.length / 2);
+  let left = nums.slice(0, mid); // 0 to mid-1
+  let right = nums.slice(mid); // mid to length
+
+  let leftSort = mergeSort(left); // sort the left half
+  let rightSort = mergeSort(right); // sort the right half
+  return merge(leftSort, rightSort);
+}
+
+function merge(first, second) {
+  let array = [];
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  while (i < first.length && j < second.length) {
+    if (first[i] < second[j]) {
+      array.push(first[i]);
+      i++;
+    } else {
+      array.push(second[j]);
+      j++;
+    }
+    k++;
+  }
+
+  while (i < first.length) {
+    array.push(first[i]);
+    i++;
+    k++;
+  }
+
+  while (j < second.length) {
+    array.push(second[j]);
+    j++;
+    k++;
+  }
+  return array;
+}
+const array = [38, 27, 43, 3, 9, 82, 10];
+console.log(mergeSort(array));
