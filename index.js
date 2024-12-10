54d5582c4144bde783a27fcc843a8b3f368029cd
Aymaan <aymaanpathan5@gmail.com>
2024-12-10 20:48:06 +0530

    Selection sort

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-function swap(nums, start, end) {
-  let temp = nums[start];
-  nums[start] = nums[end];
-  nums[end] = temp;
+function swap(nums, first, second) {
+  let temp = nums[first];
+  nums[first] = nums[second];
+  nums[second] = temp;
 }
 
-function bubble(nums) {
-  for (let i = nums.length - 1; i > 0; i--) {
-    for (let j = 0; j < i; j++) {
-      if (nums[j] > nums[j + 1]) {
-        swap(nums, j, j + 1);
-      }
+function selectionSort(nums) {
+  let min;
+  for (let i = 0; i < nums.length; i++) {
+    min = i;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] < nums[min]) min = j;
+    }
+    if (i !== min) {
+      swap(nums, i, min);
     }
   }
   return nums;
 }
-const arr = [4, 7, 6, 1, 3];
-console.log(bubble(arr));
+const nums = [4, 6, 2, 5, 3, 1];
+console.log(selectionSort(nums));
