72d4b4edb4adad09d2abe72efdbe1bbc0e78d05e
Aymaan <aymaanpathan5@gmail.com>
2024-12-12 18:01:25 +0530

    3. Longest Substring Without Repeating Characters

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-function swap(nums, first, second) {
-  let temp = nums[first];
-  nums[first] = nums[second];
-  nums[second] = temp;
-}
-
-function insertionSort(nums) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j > 0; j--) {
-      if (nums[j] < nums[j - 1]) {
-        swap(nums, j, j - 1);
-      } else {
-        break;
-      }
+var lengthOfLongestSubstring = function (s) {
+  let count = 0;
+  let str = "";
+  let max = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (!str.includes(s[i])) {
+      str += s[i];
+      count++;
+      max = Math.max(max, count);
+    } else {
+      let index = str.indexOf(s[i]);
+      str = str.slice(index + 1) + s[i];
+      count = str.length;
     }
   }
-  return nums;
-}
-const nums = [4, 6, 2, 5, 3, 1];
-console.log(insertionSort(nums));
+  return max;
+};
+const s = "bbbbb";
+console.log(lengthOfLongestSubstring(s));
