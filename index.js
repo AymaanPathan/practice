c7c365cde04390e271e597f17a87de39c87735e7
Aymaan <aymaanpathan5@gmail.com>
2024-12-13 19:16:21 +0530

    1295. Find Numbers with Even Number of Digits [toString()]

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 var findNumbers = function (nums) {
-  let count = 0;
-  let str = [];
-  for (let i = 0; i < nums.length; i++) {
-    str.push(nums[i].toString());
-  }
-  for (let i = 0; i < str.length; i++) {
-    if (str[i].length % 2 === 0) {
-      count++;
-    }
-  }
-  return count;
+  let filtered = nums.filter((item) => item.toString().length % 2 === 0);
+  return filtered.length;
 };
 const nums = [12, 345, 2, 6, 7896];
 console.log(findNumbers(nums));
