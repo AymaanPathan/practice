48a94d3e0a0e982be992fcc41bbc921122c63491
Aymaan <aymaanpathan5@gmail.com>
2024-12-13 12:12:53 +0530

    88. Merge Sorted Array

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var singleNumber = function (nums) {
-  let freq = {};
-  let ans;
-  for (let i = 0; i < nums.length; i++) {
-    let num = nums[i];
-    if (!freq[num]) {
-      freq[num] = 1;
+var merge = function (nums1, m, nums2, n) {
+  let start1 = m - 1;
+  let start2 = n - 1;
+  let i = m + n - 1;
+  while (start1 >= 0 && start2 >= 0) {
+    if (nums1[start1] > nums2[start2]) {
+      nums1[i] = nums1[start1];
+      start1--;
     } else {
-      freq[num]++;
+      nums1[i] = nums2[start2];
+      start2--;
     }
+    i--;
   }
-
-  const key = Object.keys(freq);
-  const value = Object.values(freq);
-
-  for (let i = 0; i < value.length; i++) {
-    if (value[i] === 1) return Number(key[i]);
+  while (start2 >= 0) {
+    nums1[i] = nums2[start2];
+    start2--;
+    i--;
   }
+  return nums1;
 };
