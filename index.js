f092b19a71c7b9936044d513765fd87b8af6d205
Aymaan <aymaanpathan5@gmail.com>
2024-12-13 10:28:04 +0530

    136. Single Number

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-var lengthOfLongestSubstring = function (s) {
-  let count = 0;
-  let str = "";
-  let max = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (!str.includes(s[i])) {
-      str += s[i];
-      count++;
-      max = Math.max(max, count);
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  let freq = {};
+  let ans;
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    if (!freq[num]) {
+      freq[num] = 1;
     } else {
-      let index = str.indexOf(s[i]);
-      str = str.slice(index + 1) + s[i];
-      count = str.length;
+      freq[num]++;
     }
   }
-  return max;
+
+  const key = Object.keys(freq);
+  const value = Object.values(freq);
+
+  for (let i = 0; i < value.length; i++) {
+    if (value[i] === 1) return Number(key[i]);
+  }
 };
-const s = "bbbbb";
-console.log(lengthOfLongestSubstring(s));
