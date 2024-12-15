031b082fbc47db8a0a8c6945666e260e0a90d878
Aymaan <aymaanpathan5@gmail.com>
2024-12-15 21:48:35 +0530

    905. Sort Array By Parity

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-var distributeCandies = function (candyType) {
-  let num = [];
-  let n = 0;
-  let half = Math.floor(candyType.length / 2);
-  let i = 0;
-  let count = 0;
-  while (i <= candyType.length - 1 && n < half) {
-    if (!num.includes(candyType[i])) {
-      num.push(candyType[i]);
-      n++;
-    }
-    i++;
-  }
-  for (let i = 0; i < num.length; i++) {
-    if (!num[i] !== num[i + 1]) {
-      count++;
+var sortArrayByParity = function (nums) {
+  let arr = [];
+  let k;
+  for (k = 0; k < nums.length; k++) {
+    if (nums[k] % 2 === 0) {
+      arr.push(nums[k]);
     }
   }
-  return count;
+
+  for (k = 0; k < nums.length; k++) {
+    if (nums[k] % 2) {
+      arr.push(nums[k]);
+    }
+  }
+
+  return arr;
 };
-const candyType = [6, 6, 6, 6];
-console.log(distributeCandies(candyType));
+const nums = [3, 1, 2, 4];
+console.log(sortArrayByParity(nums));
