08d75ef5fd481e1c169a6285ce1aaf7ba52d4b86
Aymaan <aymaanpathan5@gmail.com>
2024-12-16 17:08:47 +0530

    961. N-Repeated Element in Size 2N Array

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-function swap(nums, first, second) {
-  let temp = nums[first];
-  nums[first] = nums[second];
-  nums[second] = temp;
-}
-
-function bubbleSort(nums) {
-  for (let i = nums.length; i >= 0; i--) {
-    for (let j = 0; j < i; j++) {
-      if (nums[j] > nums[j + 1]) {
-        swap(nums, j, j + 1);
-      }
+var repeatedNTimes = function (nums) {
+  let freq = {};
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    if (!freq[num]) {
+      freq[num] = 1;
+    } else {
+      freq[num]++;
+    }
+    if (freq[num] >= 2) {
+      return num;
     }
   }
-  return nums;
-}
-const nums = [6, 5, 3, 2, 9, 10, 1, 34, 56, 78, 32];
-console.log(bubbleSort(nums));
+  return -1;
+};
+const nums = [5, 1, 5, 2, 5, 3, 5, 4];
+console.log(repeatedNTimes(nums));
