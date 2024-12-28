9789aab0905219d6252e71aa1f3368e5caf0db9d
aymaan <aymaanpathan5@gmail.com>
2024-12-28 14:01:37 +0530

    876. Middle of the Linked List

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-var hasCycle = function (head) {
+var middleNode = function (head) {
   let slow = head;
   let fast = head;
-  while (slow && slow.next && fast && fast.next) {
+  while (fast && fast.next) {
     slow = slow.next;
     fast = fast.next.next;
-    if (slow === fast) {
-      return true;
-    }
   }
-  return false;
+  return slow;
 };
