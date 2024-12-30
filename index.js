26a6812576c45d88afebd7c16ac4bd7d8e1f25a7
aymaan <aymaanpathan5@gmail.com>
2024-12-30 22:13:48 +0530

    142. Linked List Cycle II

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-var hasCycle = function (head) {
+function lengthCycle (head){
   let slow = head;
   let fast = head;
-  while (slow && slow.next && fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) {
-      return true;
+  let length  = 0
+  while(fast!==null && fast.next!=null){
+    slow = slow.next
+    fast = fast.next.next
+    if(slow===fast){
+      let temp = slow;
+      do{
+        temp = temp.next
+        length++
+      } while(temp!==slow)
+        return length
     }
   }
-  return false;
-};
-
-function calculateLength(meetPoint) {
-  let current = meetPoint;
-  length = 0;
-  do {
-    current = current.next;
-    length++;
-  } while (current !== meetPoint);
-  return length;
+  return 0
 }
+
+var detectCycle = function(head) {
+    let length = lengthCycle(head);
+    let second = head;
+    let first = head
+    if(length===0){
+      return null
+    }
+    while(length >=1){
+      second = second.next;
+      length--
+    }
+    while(first!==second){
+      first = first.next;
+      second = second.next
+    }
+    return first
+};
