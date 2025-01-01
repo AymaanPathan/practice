1587a0e4ce09cec60233a5816571b2180b5710d0
aymaan <aymaanpathan5@gmail.com>
2025-01-01 12:46:53 +0530

    148. Sort List [revision]

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-var reverseList = function(head) {
-    let prev = null;
-    let pres = head;
-    while(pres){
-        let next = pres.next;
-        pres.next = prev;
-        prev = pres;
-        pres = next;
+var sortList = function(head) {
+    if(!head || !head.next){
+      return head;
     }
-    return prev
+    let mid = findMid(head);
+    let left = head;
+    let right = mid.next;
+    mid.next = null
+    left = sortList(left)
+    right = sortList(right)
+    return merge(left,right)
 };
+
+function merge(l1,l2){
+  let dummy = new ListNode(0);
+  let head = dummy;
+
+  while(l1 && l2){
+    if(l1.val < l2.val){
+      dummy.next = l1;
+      l1 = l1.next;
+    } else{
+      dummy.next =l2;
+      l2 = l2.next;
+    }
+    dummy  = dummy.next
+  }
+  if(l1) dummy.next = l1
+  if(l2) dummy.next = l2
+  return head.next
+}
+
+function findMid(head){
+  let slow = head;
+  let fast = head;
+  while(fast && fast.next!==null && fast.next.next!==null){
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow
+}
