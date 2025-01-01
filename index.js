5664e8295b6df1b4fd0c1033f850341cd251716a
aymaan <aymaanpathan5@gmail.com>
2025-01-01 12:01:49 +0530

    202. Happy Number

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
-var sortList = function(head) {
-    if(!head || !head.next){
-      return head
+var isHappy = function(n) {
+    let slow = n;
+    let fast = n;
+    do{
+      slow = findSquare(slow);
+      fast = findSquare(findSquare(fast))
+    } while(slow!==fast);
+    if(slow===1){
+      return true
+    } else{
+      return false
     }
-    let mid = findMiddle(head);
-    let left = head;
-    let right = mid.next 
-    mid.next = null  
-     left = sortList(left);
-    right = sortList(right);
-    return merge(left, right);
 };
 
-function merge(l1,l2){
-  let dummy = new ListNode(0);
-  let head = dummy;
-  while(l1 && l2){
-    if(l1.val <l2.val){
-      dummy.next = l1
-      l1 = l1.next;
-    } else{
-      dummy.next = l2;
-      l2 = l2.next
-    }
-    dummy = dummy.next
-  }
-  if(l1) dummy.next = l1
-  if(l2) dummy.next = l2
-  return head.next;
+function findSquare(num){
+  let ans = 0;
+while(num>=1){
+  let lastDigit = num %10;
+  ans+= lastDigit* lastDigit;
+      num = Math.floor(num / 10);;
 }
-
-function findMiddle(head){
-  let slow = head;
-  let fast = head;
-  while(fast!==null && fast.next!==null && fast.next.next!==null){
-    slow = slow.next;
-    fast = fast.next.next
-  }
-  return slow;
+return ans
 }
+const num = 19;
+console.log(isHappy(num))
