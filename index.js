b7ef403bd3d1a0a5d6d65fcf2023bb820df16ef2
aymaan <aymaanpathan5@gmail.com>
2025-01-01 12:51:39 +0530

    lengthOfCycle  [revision]

--- index.js
+++ index.js
@@ -1,41 +1,18 @@
-var sortList = function(head) {
-    if(!head || !head.next){
-      return head;
+var lengthOfCycle = function(head) {
+    let slow = head;
+    let fast = head;
+    let len =0
+    while(fast && fast.next!==null){
+      slow = slow.next;
+      fast = fast.next.next;
+      if(slow===fast){
+        let temp = slow;
+        do{
+          temp = temp.next
+          len++
+        }while(temp!==slow)
+          return len
+      }
     }
-    let mid = findMid(head);
-    let left = head;
-    let right = mid.next;
-    mid.next = null
-    left = sortList(left)
-    right = sortList(right)
-    return merge(left,right)
+    return 0
 };
-
-function merge(l1,l2){
-  let dummy = new ListNode(0);
-  let head = dummy;
-
-  while(l1 && l2){
-    if(l1.val < l2.val){
-      dummy.next = l1;
-      l1 = l1.next;
-    } else{
-      dummy.next =l2;
-      l2 = l2.next;
-    }
-    dummy  = dummy.next
-  }
-  if(l1) dummy.next = l1
-  if(l2) dummy.next = l2
-  return head.next
-}
-
-function findMid(head){
-  let slow = head;
-  let fast = head;
-  while(fast && fast.next!==null && fast.next.next!==null){
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-  return slow
-}
