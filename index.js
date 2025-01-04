e8383a38536dc7e2127a605607181d7fe57db169
aymaan <aymaanpathan5@gmail.com>
2025-01-04 21:54:33 +0530

    insertAtEnd [without tail]

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
 class Solution {
-    getKthFromLast(head, k) {
-       head = this.reverse(head);
-      let temp = head;
-       for(let i=1;i<k;i++){
-           if(temp!==null){
-           temp = temp.next
-           } else {
-                return -1; 
-            }
-       }
-       return temp!==null ? temp.data  :-1
-    }
-    reverse(head){
-        let prev = null;
-        let pres = head;
-        while(pres){
-            let next = pres.next;
-            pres.next = prev
-            prev = pres;
-            pres = next;
+    insertAtEnd(head, x) {
+        let newNode = new Node(x)
+        if(!head){
+            head = newNode;
         }
-    return prev
+        let temp = head;
+        while(temp.next){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        newNode.next =null
+        return head;
     }
 }
