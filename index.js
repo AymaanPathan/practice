037affc4584cba27460beb18df9a3d2556f501ec
aymaan <aymaanpathan5@gmail.com>
2025-01-05 15:49:31 +0530

    del middlw

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
- findIntersection(head1, head2) {
-       let newNode = new Node(0); //head
-       let dummy = newNode 
-       let temp1  =head1;
-       let temp2 = head2;
-       
-       while(temp1 && temp2){
-        if(temp1.data < temp2.data){
-            temp1 = temp1.next
-        }  else if(temp2.data < temp1.data){
-            temp2 = temp2.next
-        } else {
-          dummy.next = new Node(temp1.data);
-            dummy = dummy.next;
-
-            // Move both pointers forward
-            temp1 = temp1.next;
-            temp2 = temp2.next;
-        }
+class Solution {
+    deleteMid(node) {
+          if (!node || !node.next) return null;
+       let slow = node;
+       let fast = node;
+       let prev = null
+       while(fast && fast.next){
+           prev = slow  // loop will end before prev became mid so we alsways  
+           slow = slow.next;
+           fast = fast.next.next;
        }
-       return newNode.next
+        prev.next = slow.next;
+        return node
     }
+}
