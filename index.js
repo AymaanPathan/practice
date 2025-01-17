e8ef17ea69c5965ea7716e107d47025ccac86886
aymaan <aymaanpathan5@gmail.com>
2025-01-17 22:06:15 +0530

    sortedMerge

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-class Solution {
-    deleteMid(node) {
-          if (!node || !node.next) return null;
-       let slow = node;
-       let fast = node;
-       let prev = null
-       while(fast && fast.next){
-           prev = slow  // loop will end before prev became mid so we alsways  
-           slow = slow.next;
-           fast = fast.next.next;
-       }
-        prev.next = slow.next;
-        return node
+  sortedMerge(head1, head2) {
+        let newNode = new Node(0);
+        let current = newNode;
+        let temp1 = head1;
+        let temp2 = head2;
+        while(temp1 && temp2){
+            if(temp1.data <=temp2.data){
+                current.next = temp1;
+                temp1 = temp1.next
+            } else{
+                current.next = temp2;
+                temp2 = temp2.next
+            }
+            current = current.next
+        }
+        if (temp1) {
+            current.next = temp1;
+        } else if (temp2) {
+            current.next = temp2;
+        }
+    return newNode.next
     }
-}
