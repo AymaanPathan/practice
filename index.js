600c964c70625f8a21d08e46d27b83c56ee8de16
aymaan <aymaanpathan5@gmail.com>
2025-01-18 21:09:09 +0530

    palindrome ll

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-  sortedMerge(head1, head2) {
-        let newNode = new Node(0);
-        let current = newNode;
-        let temp1 = head1;
-        let temp2 = head2;
-        while(temp1 && temp2){
-            if(temp1.data <=temp2.data){
-                current.next = temp1;
-                temp1 = temp1.next
-            } else{
-                current.next = temp2;
-                temp2 = temp2.next
-            }
-            current = current.next
-        }
-        if (temp1) {
-            current.next = temp1;
-        } else if (temp2) {
-            current.next = temp2;
-        }
-    return newNode.next
+ isPalindrome(head) {
+       let slow =head;
+       let fast = head;
+       while(fast && fast.next){
+           slow = slow.next;
+           fast = fast.next.next;
+       }
+       let prev = null
+       while(slow){
+            let next = slow.next;
+            slow.next = prev
+            prev = slow;
+            slow = next;
+       }
+       let left = head;
+       let right = prev;
+       while(right){
+           if(left.data!==right.data){
+               return false
+           }
+           left = left.next;
+           right = right.next
+       }
+       return true
     }
