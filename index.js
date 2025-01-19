0582c481388121bfd9d1265e69b0bea0fbe83729
aymaan <aymaanpathan5@gmail.com>
2025-01-19 22:19:34 +0530

    stack intro [valid parentheses]

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
- isPalindrome(head) {
-       let slow =head;
-       let fast = head;
-       while(fast && fast.next){
-           slow = slow.next;
-           fast = fast.next.next;
-       }
-       let prev = null
-       while(slow){
-            let next = slow.next;
-            slow.next = prev
-            prev = slow;
-            slow = next;
-       }
-       let left = head;
-       let right = prev;
-       while(right){
-           if(left.data!==right.data){
-               return false
-           }
-           left = left.next;
-           right = right.next
-       }
-       return true
+ isParenthesisBalanced(s) {
+      let stack = new Stack();
+      for(let i=0;i<s.length;i++){
+          let char = s[i];
+          if(char=="("){
+              stack.push(")")
+          }else if(char==="["){
+              stack.push("]")
+          }else if(char==="{"){
+              stack.push("}")
+          }  else if (stack.pop() !== char) {
+                    return false;
+
+            }
+      }
+      return stack.isEmpty()
     }
