143dfb472dcd26422f4455021f9c2a023dc38bb9
aymaan <aymaanpathan5@gmail.com>
2025-01-20 21:35:14 +0530

    left greater element

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
- nextLargerElement(arr) {
-        let stack = new Stack();
-         let nums = [];
-       for(let i=arr.length-1;i>=0;i--){
-         while(!stack.isEmpty() && stack.peek()<=arr[i]){
-             stack.pop();
-         }
-           if(!stack.isEmpty()){
-               nums.push(stack.peek());
-           }else{
-               nums.push(-1)
-           }
-           stack.push(arr[i])
-       }
-        return nums.reverse()
+function nearestLargerToLeft(arr) {
+  let stack = new Stack();
+  let nums = [];
+  for (let i = 0; i < arr.length; i++) {
+    while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+      stack.pop();
     }
+    if (!stack.isEmpty()) {
+      nums.push(stack.peek());
+    } else {
+      nums.push(-1);
+    }
+    stack.push(arr[i]);
+  }
+  return nums.reverse();
+}
