9903888206cfdf17f3e42e6df5fc9e9babca0417
aymaan <aymaanpathan5@gmail.com>
2025-01-20 21:21:48 +0530

    next greater element

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
- isParenthesisBalanced(s) {
-      let stack = new Stack();
-      for(let i=0;i<s.length;i++){
-          let char = s[i];
-          if(char=="("){
-              stack.push(")")
-          }else if(char==="["){
-              stack.push("]")
-          }else if(char==="{"){
-              stack.push("}")
-          }  else if (stack.pop() !== char) {
-                    return false;
-
-            }
-      }
-      return stack.isEmpty()
+ nextLargerElement(arr) {
+        let stack = new Stack();
+         let nums = [];
+       for(let i=arr.length-1;i>=0;i--){
+         while(!stack.isEmpty() && stack.peek()<=arr[i]){
+             stack.pop();
+         }
+           if(!stack.isEmpty()){
+               nums.push(stack.peek());
+           }else{
+               nums.push(-1)
+           }
+           stack.push(arr[i])
+       }
+        return nums.reverse()
     }
