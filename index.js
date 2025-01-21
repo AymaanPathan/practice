c6eef2e8076f63c3069ded573c231f3ecacd31b1
aymaan <aymaanpathan5@gmail.com>
2025-01-21 22:52:57 +0530

    calculateSpan

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-function nearestSmallerToLeft(arr) {
-  let stack = new Stack();
-  let nums = [];
-  for (let i = 0; i < arr.length; i++) {
-    while (!stack.isEmpty() && stack.peek() >= arr[i]) {
-      stack.pop();
+ calculateSpan(arr) {
+    let stack = new Stack();
+    const nums = [];
+
+    for(let i=0;i<arr.length;i++){
+        while(!stack.isEmpty() && arr[i]>=stack.peek().val){
+            stack.pop()
+        }
+        if(!stack.isEmpty()){
+            nums.push(stack.peek().index);
+        }else{
+            nums.push(-1)
+        }
+        stack.push(arr[i],i)
     }
-    if (!stack.isEmpty()) {
-      nums.push(stack.peek());
-    } else {
-      nums.push(-1);
-    }
-    stack.push(arr[i]);
+    return nums;
   }
-  return nums.reverse();
-}
