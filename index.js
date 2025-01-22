0ca3abc5109be45cf774d0ac8fa62a65f8d6f5ea
aymaan <aymaanpathan5@gmail.com>
2025-01-22 22:33:39 +0530

    min using stack [extra space]

--- index.js
+++ index.js
@@ -1,51 +1,86 @@
-class Node{
-    constructor(val){
-        this.val  = val;
-        this.next =null
-    }
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
-class Stack{
-    constructor(val){
-        this.top =null;
-        this.length=0;
+class Stack {
+  constructor() {
+    this.top = null;
+    this.length = 0;
+    this.ss = new SurportingStack();
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.top) {
+      this.top = newNode;
     }
-    
-    push(val){
-        let newNode = new Node(val);
-        if(!this.top){
-            this.top = newNode;
-        }
-        newNode.next = this.top;
-        this.top =newNode;
-        this.length++;
+    newNode.next = this.top;
+    this.top = newNode;
+    this.length++;
+    if (this.ss.length === 0 || this.ss.peek() >= val) {
+      this.ss.push(val);
     }
-    
-    pop(){
-        if(!this.top){
-            return undefined;
-        }
-        let temp = this.top;
-        this.top = this.top.next;
-        temp.next =null;
-        this.length--
-        return temp.val
+  }
+
+  pop() {
+    if (!this.top) {
+      return undefined;
     }
-    isEmpty(){
-        return this.length===0
+    let temp = this.top;
+    this.top = this.top.next;
+    temp.next = null;
+    this.length--;
+    if (this.ss.peek() === temp.val) {
+      this.ss.pop();
     }
+    return temp.val;
+  }
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  getMin() {
+    return this.ss.peek();
+  }
 }
 
-class Solution {
-    reverse(S){
-    let str=""
-      let stack = new Stack();
-      for(let i=0;i<S.length;i++){
-          stack.push(S[i])
-      }
-    while(!stack.isEmpty()){
-        str+=stack.pop()
+class SurportingStack {
+  constructor() {
+    this.top = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.top) {
+      this.top = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
     }
-    return str
-    }
+    this.length++;
+  }
+
+  pop() {
+    if (!this.top) return null;
+    let poppedNode = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return poppedNode.val;
+  }
+
+  peek() {
+    return this.top ? this.top.val : null;
+  }
 }
+
+const stack = new Stack();
+stack.push(5);
+stack.push(3);
+stack.push(7);
+stack.push(2);
+
+console.log(stack.getMin());
