cca4d9fa5cf02e84db9f63f969cc4dd6de585173
aymaan <aymaanpathan5@gmail.com>
2025-01-22 21:30:05 +0530

    reverse using stack

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
- calculateSpan(arr) {
-    let stack = new Stack();
-    const nums = [];
-
-    for(let i=0;i<arr.length;i++){
-        while(!stack.isEmpty() && arr[i]>=stack.peek().val){
-            stack.pop()
-        }
-        if(!stack.isEmpty()){
-            nums.push(stack.peek().index);
-        }else{
-            nums.push(-1)
-        }
-        stack.push(arr[i],i)
+class Node{
+    constructor(val){
+        this.val  = val;
+        this.next =null
     }
-    return nums;
-  }
+}
+
+class Stack{
+    constructor(val){
+        this.top =null;
+        this.length=0;
+    }
+    
+    push(val){
+        let newNode = new Node(val);
+        if(!this.top){
+            this.top = newNode;
+        }
+        newNode.next = this.top;
+        this.top =newNode;
+        this.length++;
+    }
+    
+    pop(){
+        if(!this.top){
+            return undefined;
+        }
+        let temp = this.top;
+        this.top = this.top.next;
+        temp.next =null;
+        this.length--
+        return temp.val
+    }
+    isEmpty(){
+        return this.length===0
+    }
+}
+
+class Solution {
+    reverse(S){
+    let str=""
+      let stack = new Stack();
+      for(let i=0;i<S.length;i++){
+          stack.push(S[i])
+      }
+    while(!stack.isEmpty()){
+        str+=stack.pop()
+    }
+    return str
+    }
+}
