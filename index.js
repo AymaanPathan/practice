338576d6bece455528a0ff085414f91551d1c45f
aymaan <aymaanpathan5@gmail.com>
2025-01-23 23:11:01 +0530

    revision

--- index.js
+++ index.js
@@ -8,20 +8,21 @@ class Node {
 class Stack {
   constructor() {
     this.top = null;
+    this.ss = new Stack2();
     this.length = 0;
-    this.ss = new SurportingStack();
   }
-
   push(val) {
     let newNode = new Node(val);
     if (!this.top) {
       this.top = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
     }
-    newNode.next = this.top;
-    this.top = newNode;
     this.length++;
-    if (this.ss.length === 0 || this.ss.peek() >= val) {
-      this.ss.push(val);
+
+    if (this.ss.length === 0 || val <= this.ss.peek()) {
+      this.ss.push(newNode);
     }
   }
 
@@ -33,26 +34,25 @@ class Stack {
     this.top = this.top.next;
     temp.next = null;
     this.length--;
-    if (this.ss.peek() === temp.val) {
+    if (temp.val === this.ss.peek()) {
       this.ss.pop();
     }
     return temp.val;
   }
-  isEmpty() {
-    return this.length === 0;
-  }
 
   getMin() {
+    if (!this.ss.top) {
+      return -1;
+    }
     return this.ss.peek();
   }
 }
 
-class SurportingStack {
+class Stack2 {
   constructor() {
     this.top = null;
     this.length = 0;
   }
-
   push(val) {
     let newNode = new Node(val);
     if (!this.top) {
@@ -63,24 +63,23 @@ class SurportingStack {
     }
     this.length++;
   }
-
   pop() {
-    if (!this.top) return null;
-    let poppedNode = this.top;
+    if (!this.top) {
+      return undefined;
+    }
+    let temp = this.top;
     this.top = this.top.next;
+    temp.next = null;
     this.length--;
-    return poppedNode.val;
+    return temp.val;
   }
-
   peek() {
     return this.top ? this.top.val : null;
   }
 }
+const s = new Stack();
+s.push(1);
+s.push(2);
+s.push(3);
 
-const stack = new Stack();
-stack.push(5);
-stack.push(3);
-stack.push(7);
-stack.push(2);
-
-console.log(stack.getMin());
+console.log(s.getMin());
