a88a0ebcf00e22fbd2071cd8a9530fa5f998ffb3
aymaan <aymaanpathan5@gmail.com>
2025-01-27 21:02:14 +0530

    reverse str using stack

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-var isValid = function(s) {
-    let stack =[];
-    for(let i=0;i<s.length;i++){
-        let chr = s[i];
-        if(chr==="("){
-            stack.push(")")
-        }else if(chr==="{"){
-            stack.push("}")
-        }else if(chr==="["){
-            stack.push("]")
-        }else{
-            if(stack.pop() !== chr){
-                return false
-            }
-        }
-    }
-   return stack.length===0
-};
+function reverseStringUsingStack(str) {
+  // Initialize an empty stack
+  let stack = [];
+
+  // Push all characters of the string onto the stack
+  for (let char of str) {
+    stack.push(char);
+  }
+
+  // Pop characters from the stack to reverse
+  let reversedStr = "";
+  while (stack.length > 0) {
+    reversedStr += stack.pop();
+  }
+
+  return reversedStr;
+}
+
+// Test the function
+const inputStr = "hello";
+console.log("Original String:", inputStr);
+console.log("Reversed String:", reverseStringUsingStack(inputStr));
