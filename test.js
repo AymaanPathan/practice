1fda83c907693f59d9c80c94d8dd5521a2399707
aymaan <aymaanpathan5@gmail.com>
2025-02-04 22:12:15 +0530

    49. Group Anagrams

--- test.js
+++ test.js
@@ -1,20 +1,15 @@
-var topKFrequent = function (nums, k) {
-  let freq = {};
-  let ans = [];
-  for (let i = 0; i < nums.length; i++) {
-    let num = nums[i];
-    if (!freq[num]) {
-      freq[num] = 1;
-    } else {
-      freq[num]++;
-    }
-  }
+var groupAnagrams = function (strs) {
+  let occur = {};
 
-  const entries = Object.entries(freq).sort((a, b) => b[1] - a[1]);
-  for (let i = 0; i < k; i++) {
-    ans.push(parseInt(entries[i][0]));
+  for (let i = 0; i < strs.length; i++) {
+    let chr = strs[i].split("").sort().join("");
+    if (!occur[chr]) {
+      occur[chr] = [];
+    }
+    occur[chr].push(strs[i]);
   }
-  return ans;
+  return occur;
 };
-const nums = [1, 1, 1, 2, 2, 3];
-console.log(topKFrequent(nums, 2));
+
+const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+console.log(groupAnagrams(strs));
