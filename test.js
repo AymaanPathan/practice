a68e1b5afcda94617afa180b8e50124d8bed0232
aymaan <aymaanpathan5@gmail.com>
2025-03-04 14:49:01 +0530

    call q3

--- test.js
+++ test.js
@@ -1,14 +1,23 @@
-// Output based Question -1
-// What is the output of this code the answer is 24 because?
-// the call method will look for age in person2 because we say call getAge in person2
-// but if we remove call method it will give 20
-const myName = {
-  name: "Aymaan",
-  age: 20,
-  getAge: function () {
-    return this.age;
-  },
-};
+// Output based Question -2
+// Get All Spices And Name Of An Animal by using call method
 
-const person2 = { age: 1 };
-console.log(myName.getAge.call(person2)); // this will check person2
+const animal = [
+  {
+    species: "Lion",
+    name: "King",
+  },
+  {
+    species: "Whale",
+    name: "Queen",
+  },
+];
+function printAnimals(i) {
+  this.print = function () {
+    console.log(i + " " + this.species + " " + this.name);
+  };
+  this.print();
+}
+
+for (let i = 0; i < animal.length; i++) {
+  printAnimals.call(animal[i], i);
+}
