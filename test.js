7177bcaa5fc33c918554e60ecad1e902d7b25470
aymaan <aymaanpathan5@gmail.com>
2025-03-04 11:17:49 +0530

    call method good way

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 const myName = {
   firstName: "aymaan",
   lastName: "pathan",
-  printFullName: function () {
-    return `${this.firstName} ${this.lastName}`;
-  },
 };
 
+const printFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
 const name2 = {
   firstName: "Jack",
   lastName: "Jackson",
 };
 
 // Function borrowing
-console.log(myName.printFullName.call(myName));
+console.log(printFullName.call(name2));
