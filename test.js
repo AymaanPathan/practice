6bfec8b0f230ff9d1713ee17076a595e303d4a4b
aymaan <aymaanpathan5@gmail.com>
2025-03-12 15:20:18 +0530

    add reverseOnlyLetters

--- test.js
+++ test.js
@@ -1,23 +1,19 @@
-// Output based Question -2
-// Get All Spices And Name Of An Animal by using call method
-
-const animal = [
-  {
-    species: "Lion",
-    name: "King",
-  },
-  {
-    species: "Whale",
-    name: "Queen",
-  },
-];
-function printAnimals(i) {
-  this.print = function () {
-    console.log(i + " " + this.species + " " + this.name);
-  };
-  this.print();
-}
-
-for (let i = 0; i < animal.length; i++) {
-  printAnimals.call(animal[i], i);
-}
+var reverseOnlyLetters = function (s) {
+  let answer = "";
+  let checkCount = 0;
+  let onlyLetters = s
+    .split(/[^A-Za-z]+/)
+    .join("")
+    .split("")
+    .reverse();
+  for (let i = 0; i < s.length; i++) {
+    if ((s[i] >= "A" && s[i] <= "Z") || (s[i] >= "a" && s[i] <= "z")) {
+      answer += onlyLetters[checkCount];
+      checkCount++;
+    } else {
+      answer += s[i];
+    }
+  }
+  return answer;
+};
+console.log(reverseOnlyLetters("ab-cd"));
