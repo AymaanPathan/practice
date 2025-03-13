0b53c7c64ec2bf109cd13068424a1a525a256a7e
aymaan <aymaanpathan5@gmail.com>
2025-03-13 21:49:47 +0530

    sliding window

--- test.js
+++ test.js
@@ -1,19 +1,20 @@
-var reverseOnlyLetters = function (s) {
-  let answer = "";
-  let checkCount = 0;
-  let onlyLetters = s
-    .split(/[^A-Za-z]+/)
-    .join("")
-    .split("")
-    .reverse();
-  for (let i = 0; i < s.length; i++) {
-    if ((s[i] >= "A" && s[i] <= "Z") || (s[i] >= "a" && s[i] <= "z")) {
-      answer += onlyLetters[checkCount];
-      checkCount++;
-    } else {
-      answer += s[i];
-    }
+function findSubarraysSlidingWindow(arr, k) {
+  let window = [];
+  let ans = [];
+  for (let i = 0; i < k; i++) {
+    window.push(arr[i]);
   }
-  return answer;
-};
-console.log(reverseOnlyLetters("ab-cd"));
+  ans.push([...window]);
+
+  for (let i = k; i < arr.length; i++) {
+    window.shift();
+    window.push(arr[i]);
+    ans.push([...window]);
+  }
+  return ans;
+}
+
+// Example usage:
+let arr = [1, 2, 3, 4, 5];
+let k = 2;
+console.log(findSubarraysSlidingWindow(arr, k));
