069f7e9fa7005792c937b13fa1e90a26b902bd04
aymaan <aymaanpathan5@gmail.com>
2025-03-14 11:30:51 +0530

    sliding window max sub array

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
-function slidingWindowSubArray(nums, k) {
+function maxSubArray(nums, k) {
   let ans = [];
   let start = 0;
   let end = 0;
+  let sum = 0;
+  let max = 0;
   while (end < nums.length) {
+    sum = sum + nums[end];
     if (end - start + 1 < k) {
       end++;
     } else if (end - start + 1 === k) {
-      let sub = [];
-      for (let i = start; i <= end; i++) {
-        sub.push(nums[i]);
-      }
-      ans.push(sub);
-      start++;
-      end++;
+      max = Math.max(max, sum); // found window first time
+      sum = sum - nums[start]; // remove first element after finding first max[max for now]
+      start++; // move i after finding first sum
+      end++; // move i after finding first sum
     }
   }
-  return ans;
+  return max;
 }
 const nums = [1, 2, 3, 4, 5, 6];
-console.log(slidingWindowSubArray(nums, 2));
+console.log(maxSubArray(nums, 2));
