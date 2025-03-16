a7added00632e3555251cc1f3ddcfba46c3dc4da
aymaan <aymaanpathan5@gmail.com>
2025-03-16 17:43:43 +0530

    findMaxAverage

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
-function firstNegativeInEveryWindow(nums, k) {
+var findMaxAverage = function (nums, k) {
   let start = 0;
   let end = 0;
-  let allNegatives = [];
-  let ans = [];
+  let max = -Infinity;
+  let sum = 0;
   while (end < nums.length) {
-    if (nums[end] < 0) {
-      allNegatives.push(nums[end]);
-    }
+    sum = sum + nums[end];
     if (end - start + 1 < k) {
       end++;
-    } else {
-      if (allNegatives.length == 0) {
-        ans.push(0);
-      } else {
-        ans.push(allNegatives[0]);
-        if (nums[start] < 0) {
-          allNegatives.shift();
-        }
-      }
+    } else if (end - start + 1 === k) {
+      max = Math.max(max, sum / k);
+      sum = sum - nums[start];
       start++;
       end++;
     }
   }
-  return ans;
-}
-const arr = [12, -1, -7, 8, -15, 30, 16, 28];
-const k = 3;
-console.log(firstNegativeInEveryWindow(arr, k));
+  return max;
+};
