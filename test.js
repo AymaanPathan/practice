d775e2db6fb2f0d8783d4d585e8392e4d3937de8
aymaan <aymaanpathan5@gmail.com>
2025-03-20 23:19:31 +0530

    CountEvenNumbersinEveryWindow

--- test.js
+++ test.js
@@ -1,21 +1,23 @@
-function AllSubarraysofSizeK(nums, k) {
+function CountEvenNumbersinEveryWindow(nums, k) {
   let start = 0;
   let end = 0;
-  let sum = 0;
+  let count = 0;
   let ans = [];
   while (end < nums.length) {
-    sum = sum + nums[end];
+    if (nums[end] % 2 === 0) {
+      count++;
+    }
     if (end - start + 1 < k) {
       end++;
     } else if (end - start + 1 === k) {
-      ans.push(sum / k);
-      sum = sum - nums[start];
+      ans.push(count);
+      if (nums[start] % 2 === 0) count--;
       start++;
       end++;
     }
   }
   return ans;
 }
-const nums = [2, 4, 6, 8, 10];
-const k = 2;
-console.log(AllSubarraysofSizeK(nums, k));
+const nums = [1, 2, 3, 4, 5];
+const k = 3;
+console.log(CountEvenNumbersinEveryWindow(nums, k));
