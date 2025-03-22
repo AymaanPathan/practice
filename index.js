e50e65a7d51394ec294712d6c26ff2259cb7aabc
aymaan <aymaanpathan5@gmail.com>
2025-03-22 11:22:54 +0530

    minSubArrayLen

--- index.js
+++ index.js
@@ -1,40 +1,12 @@
-function maxSlidingWindow(nums, k) {
-  let result = [];
-  let deque = []; // Useful indexes store karega
-  let start = 0,
-    end = 0; // Start and End window ka track rakhenge
-
+function MaximumSubarray(nums, k) {
+  let start = 0;
+  let end = 0;
+  let sum = 0;
+  let max = nums.reduce((acc, curr) => acc + curr, 0);
   while (end < nums.length) {
-    // Step 1: Chhoti values hatao jo naye element se chhoti hain
-    while (deque.length > 0 && nums[deque[deque.length - 1]] <= nums[end]) {
-      deque.pop();
-    }
-
-    // Step 2: Current element ka index add karo
-    deque.push(end);
-
-    // Step 3: Window size check karo
-    if (end - start + 1 == k) {
-      // Window full ho gayi?
-      result.push(nums[deque[0]]); // Max element (front of deque)
-
-      // Step 4: Agar front index window se bahar ho gaya, hatao
-      if (deque[0] == start) {
-        deque.shift();
-      }
-
-      // Step 5: Window ko slide karo
-      start++;
-    }
-
-    // Move end forward
-    end++;
+    sum = sum + nums[j];
   }
-
-  return result;
 }
-
-// Example Usage
-let nums = [1, 3, -1, -3, 5, 3, 6, 7];
-let k = 3;
-console.log(maxSlidingWindow(nums, k));
+const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const k = 3;
+console.log(MaximumSubarray(nums, k));
--- test.js
+++ test.js
@@ -1,23 +1,31 @@
-function CountEvenNumbersinEveryWindow(nums, k) {
+var minSubArrayLen = function (target, nums) {
   let start = 0;
   let end = 0;
-  let count = 0;
-  let ans = [];
+  let minSize = Infinity;
+  let sum = 0;
   while (end < nums.length) {
-    if (nums[end] % 2 === 0) {
-      count++;
-    }
-    if (end - start + 1 < k) {
+    sum = sum + nums[end];
+    if (sum < target) {
       end++;
-    } else if (end - start + 1 === k) {
-      ans.push(count);
-      if (nums[start] % 2 === 0) count--;
-      start++;
+    } else if (sum === target) {
+      if (minSize > end - start + 1) {
+        minSize = end - start + 1;
+      }
+      end++;
+    }
+    if (sum > target) {
+      while (sum >= target) {
+        if (minSize > end - start + 1) {
+          minSize = end - start + 1;
+        }
+        sum = sum - nums[start];
+        start++;
+      }
       end++;
     }
   }
-  return ans;
-}
-const nums = [1, 2, 3, 4, 5];
-const k = 3;
-console.log(CountEvenNumbersinEveryWindow(nums, k));
+  return minSize === Infinity ? 0 : minSize;
+};
+const nums = [2, 3, 1, 2, 4, 3];
+const target = 7;
+console.log(minSubArrayLen(target, nums));
