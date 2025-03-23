5b639fc9c56c7f12f6a9fb532f2e409b98e7d3f0
aymaan <aymaanpathan5@gmail.com>
2025-03-23 12:59:29 +0530

    numOfSubarrays

--- test.js
+++ test.js
@@ -1,28 +1,25 @@
-var CountWindowswithSumatLeastTarget = function (nums, k, target) {
+var numOfSubarrays = function (arr, k, threshold) {
   let start = 0;
   let end = 0;
   let sum = 0;
   let ans = 0;
-  while (end < nums.length) {
-    sum = sum + nums[end];
+  while (end < arr.length) {
+    sum = sum + arr[end];
     if (end - start + 1 < k) {
       end++;
     } else if (end - start + 1 === k) {
-      if (sum >= target) {
+      if (sum / k >= threshold) {
         ans++;
       }
-      sum = sum - nums[start];
+      sum = sum - arr[start];
       start++;
       end++;
     }
   }
   return ans;
 };
-const nums = [1, 2, 3, 1];
-const k = 2;
-const target = 10;
-console.log(CountWindowswithSumatLeastTarget([1, 2, 3, 1], 2, 10)); // Expected output: 0
-console.log(CountWindowswithSumatLeastTarget([2, 1, 5, 1, 3, 2], 3, 7)); // Expected output: 3
-console.log(CountWindowswithSumatLeastTarget([4, 4, 4, 4], 2, 8)); // Expected output: 3
-console.log(CountWindowswithSumatLeastTarget([100, 200, 300, 400], 2, 500)); // Expected output: 2
-console.log(CountWindowswithSumatLeastTarget([3, 3, 3], 3, 9)); // Expected output: 1
+
+const arr = [11, 13, 17, 23, 29, 31, 7, 5, 2, 3];
+const k = 3;
+const threshold = 5;
+console.log(numOfSubarrays(arr, k, threshold));
