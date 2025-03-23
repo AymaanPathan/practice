5d4504f8bb22724fb7011b3a9b03765b2653934a
aymaan <aymaanpathan5@gmail.com>
2025-03-23 12:47:09 +0530

    CountWindowswithSumatLeastTarget

--- test.js
+++ test.js
@@ -1,31 +1,28 @@
-var maxSlidingWindow = function (nums, k) {
+var CountWindowswithSumatLeastTarget = function (nums, k, target) {
   let start = 0;
   let end = 0;
-  let ans = [];
-  let maxArray = [];
+  let sum = 0;
+  let ans = 0;
   while (end < nums.length) {
-    while (
-      maxArray.length > 0 &&
-      nums[maxArray[maxArray.length - 1]] < nums[end]
-    ) {
-      maxArray.pop();
-    }
-    maxArray.push(end);
-
+    sum = sum + nums[end];
     if (end - start + 1 < k) {
       end++;
-    } else if (end - start + 1 == k) {
-      ans.push(nums[maxArray[0]]);
-      if (maxArray[0] === start) {
-        maxArray.shift();
+    } else if (end - start + 1 === k) {
+      if (sum >= target) {
+        ans++;
       }
+      sum = sum - nums[start];
       start++;
       end++;
     }
   }
   return ans;
 };
-
-let nums = [1, 3, -1, -3, 5, 3, 6, 7];
-let k = 3;
-console.log(maxSlidingWindow(nums, k));
+const nums = [1, 2, 3, 1];
+const k = 2;
+const target = 10;
+console.log(CountWindowswithSumatLeastTarget([1, 2, 3, 1], 2, 10)); // Expected output: 0
+console.log(CountWindowswithSumatLeastTarget([2, 1, 5, 1, 3, 2], 3, 7)); // Expected output: 3
+console.log(CountWindowswithSumatLeastTarget([4, 4, 4, 4], 2, 8)); // Expected output: 3
+console.log(CountWindowswithSumatLeastTarget([100, 200, 300, 400], 2, 500)); // Expected output: 2
+console.log(CountWindowswithSumatLeastTarget([3, 3, 3], 3, 9)); // Expected output: 1
