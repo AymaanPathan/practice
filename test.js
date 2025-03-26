a6d59d9a483be5ccefb4c2486173b9c31fc2cbae
aymaan <aymaanpathan5@gmail.com>
2025-03-26 23:10:10 +0530

    updated

--- test.js
+++ test.js
@@ -1,29 +1,30 @@
 var SmallestSubarraywithSumGreatorOrEqualTarget = function (nums, target) {
   let start = 0;
   let end = 0;
-  let ans = Infinity;
+  let ans = [];
   let sum = 0;
-  let arr = [];
+  let min = Infinity;
   while (end < nums.length) {
     sum = sum + nums[end];
     if (sum < target) {
       end++;
     } else if (sum >= target) {
       while (sum >= target) {
-        if (ans > end - start + 1) {
-          ans = end - start + 1; // new answer found
-          arr = []; // empty arr after new answer found
+        if (end - start + 1 < min) {
+          min = end - start + 1;
+          ans = [];
           for (let i = start; i <= end; i++) {
-            arr.push(nums[i]);
+            ans.push(nums[i]);
           }
         }
         sum = sum - nums[start];
         start++;
+        // end  ->end is agge than start we cant do this
       }
       end++;
     }
   }
-  return arr;
+  return ans;
 };
 const nums = [2, 3, 1, 2, 4, 3];
 const target = 7;
