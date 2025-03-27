a59758ab2224acd681e07782ef5342863f29fc3a
aymaan <aymaanpathan5@gmail.com>
2025-03-27 22:18:50 +0530

    MaximumSumofAnySubarrayofSizeAtMostK

--- test.js
+++ test.js
@@ -1,36 +1,22 @@
-var LongestSubstringwithAtMostKDistinctCharacters = function (s, k) {
+var MaximumSumofAnySubarrayofSizeAtMostK = function (nums, k) {
   let start = 0;
   let end = 0;
-  let freq = {};
-  let max = -Infinity;
-  while (end < s.length) {
-    let chr = s[end];
-    if (!freq[chr]) {
-      freq[chr] = 1;
-    } else {
-      freq[chr]++;
-    }
-    if (Object.keys(freq).length < k) {
-      max = Math.max(max, end - start + 1); // edge case
+  let sum = 0;
+  let ans = 0;
+  while (end < nums.length) {
+    sum = sum + nums[end];
+    if (end - start + 1 < k) {
       end++;
-    } else if (Object.keys(freq).length === k) {
-      max = Math.max(max, end - start + 1);
-      end++;
-    } else if (Object.keys(freq).length > k) {
-      while (Object.keys(freq).length > k) {
-        let sChar = s[start];
-        freq[sChar]--;
-        if (freq[sChar] === 0) {
-          delete freq[sChar];
-        }
-        start++;
-      }
+    } else if (end - start + 1 === k) {
+      ans = Math.max(ans, sum);
+      sum -= nums[start];
+      start++;
       end++;
     }
   }
-  return max;
+  return ans;
 };
-const s = "abc";
-const k = 5;
+const nums = [3, 2, 5, 4, 1];
+const k = 2;
 
-console.log(LongestSubstringwithAtMostKDistinctCharacters(s, k));
+console.log(MaximumSumofAnySubarrayofSizeAtMostK(nums, k));
