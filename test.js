5be2941111f017e5e7c87e066ef719c44e66a852
aymaan <aymaanpathan5@gmail.com>
2025-04-04 22:44:56 +0530

    recursion 1 to N

--- test.js
+++ test.js
@@ -1,47 +1,8 @@
-var FindAllAnagrams = function (s, p) {
-  let start = 0;
-  let end = 0;
-  let ans = 0;
-  let freq = {};
-
-  for (let i = 0; i < p.length; i++) {
-    let chr = p[i];
-    if (!freq[chr]) {
-      freq[chr] = 1;
-    } else {
-      freq[chr]++;
-    }
+function printOneToN(n) {
+  if (n === 0) {
+    return;
   }
-
-  let count = Object.keys(freq).length;
-
-  while (end < s.length) {
-    let chr = s[end];
-    if (chr in freq) {
-      freq[chr]--;
-      if (freq[chr] === 0) {
-        count--;
-      }
-    }
-    if (end - start + 1 === p.length) {
-      if (count === 0) {
-        ans++;
-      }
-      let startChar = s[start];
-      if (startChar in freq) {
-        if (freq[startChar] === 0) {
-          count++;
-        }
-        freq[startChar]++;
-      }
-      start++;
-    }
-    end++;
-  }
-
-  return ans;
-};
-const s = "cbaebabacd";
-const p = "abc";
-
-console.log(FindAllAnagrams(s, p));
+  printOneToN(n - 1);
+  console.log(n);
+}
+printOneToN(7);
