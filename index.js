73f283b98716f4e7a74b2a48a6e285764f5f5f8d
aymaan <aymaanpathan5@gmail.com>
2025-04-11 23:25:40 +0530

    20. Valid Parentheses

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
-function StockSpan(nums) {
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
   let stack = [];
-  let nearestGreatestToLeftIndexArr = [];
-  let ans = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (stack.length === 0) {
-      // If nothing on left side
-      nearestGreatestToLeftIndexArr.push(-1);
-    }
-    // If has left side but smaller
-    else if (stack.length > 0 && stack[stack.length - 1].element <= nums[i]) {
-      while (stack.length > 0 && stack[stack.length - 1].element <= nums[i]) {
-        stack.pop();
-      }
-      if (stack.length === 0) {
-        nearestGreatestToLeftIndexArr.push(-1);
-      } else {
-        nearestGreatestToLeftIndexArr.push(stack[stack.length - 1].index);
+  for (let i = 0; i < s.length; i++) {
+    let chr = s[i];
+    if (chr === "(") {
+      stack.push(")");
+    } else if (chr === "{") {
+      stack.push("}");
+    } else if (chr === "[") {
+      stack.push("]");
+    } else {
+      if (stack.pop() !== chr) {
+        return false;
       }
     }
-    // Finaly we get larger on  left
-    else {
-      nearestGreatestToLeftIndexArr.push(stack[stack.length - 1].index);
-    }
-    stack.push({ element: nums[i], index: i });
   }
-  for (let i = 0; i < nearestGreatestToLeftIndexArr.length; i++) {
-    ans[i] = i - nearestGreatestToLeftIndexArr[i];
-  }
-  return ans;
-}
-const nums = [100, 80, 60, 70, 60, 75, 85];
-console.log(StockSpan(nums));
+  return stack.length === 0;
+};
