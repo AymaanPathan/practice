feb2ac796bf857e2f8aeaf570abdb0db415aeea4
aymaan <aymaanpathan5@gmail.com>
2025-04-11 23:07:36 +0530

    nearestGreatestToLeftIndex

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-function MaximumSubarray(nums, k) {
-  let start = 0;
-  let end = 0;
-  let sum = 0;
-  let max = nums.reduce((acc, curr) => acc + curr, 0);
-  while (end < nums.length) {
-    sum = sum + nums[j];
+function nearestGreatestToLeftIndex(nums) {
+  let stack = [];
+  let nearestGreatestToLeftIndexArr = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (stack.length === 0) {
+      // If nothing on left side
+      nearestGreatestToLeftIndexArr.push(-1);
+    }
+    // If has left side but smaller
+    else if (stack.length > 0 && stack[stack.length - 1].element <= nums[i]) {
+      while (stack.length > 0 && stack[stack.length - 1].element <= nums[i]) {
+        stack.pop();
+      }
+      if (stack.length === 0) {
+        nearestGreatestToLeftIndexArr.push(-1);
+      } else {
+        nearestGreatestToLeftIndexArr.push(stack[stack.length - 1].index);
+      }
+    }
+    // Finaly we get larger on left
+    else {
+      nearestGreatestToLeftIndexArr.push(stack[stack.length - 1].index);
+    }
+    stack.push({ element: nums[i], index: i });
   }
+  return nearestGreatestToLeftIndexArr;
 }
-const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-const k = 3;
-console.log(MaximumSubarray(nums, k));
+const nums = [100, 80, 60, 70, 60, 75, 85];
+console.log(nearestGreatestToLeftIndex(nums));
