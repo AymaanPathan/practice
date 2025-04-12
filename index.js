9c3275d2209ae90a2edfac23f6efa7bf930eacf9
aymaan <aymaanpathan5@gmail.com>
2025-04-13 00:05:35 +0530

    maximum area of the histogram

--- index.js
+++ index.js
@@ -1,22 +1,70 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function (s) {
+// Find nearest smllest to left
+function nearestsmllesttoleft(nums) {
   let stack = [];
-  for (let i = 0; i < s.length; i++) {
-    let chr = s[i];
-    if (chr === "(") {
-      stack.push(")");
-    } else if (chr === "{") {
-      stack.push("}");
-    } else if (chr === "[") {
-      stack.push("]");
-    } else {
-      if (stack.pop() !== chr) {
-        return false;
+  let index = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (stack.length === 0) {
+      index.push(-1);
+    } else if (stack.length > 0 && stack[stack.length - 1].element >= nums[i]) {
+      while (stack.length > 0 && stack[stack.length - 1].element >= nums[i]) {
+        stack.pop();
       }
+      if (stack.length === 0) {
+        index.push(-1);
+      } else {
+        index.push(stack[stack.length - 1].index);
+      }
+    } else {
+      index.push(stack[stack.length - 1].index);
     }
+    stack.push({ element: nums[i], index: i });
   }
-  return stack.length === 0;
-};
+  return index;
+}
+
+function nearestsmllesttoRight(nums) {
+  let stack = [];
+  let index = [];
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (stack.length === 0) {
+      index.push(nums.length);
+    } else if (stack.length > 0 && stack[stack.length - 1].element >= nums[i]) {
+      while (stack.length > 0 && stack[stack.length - 1].element >= nums[i]) {
+        stack.pop();
+      }
+      if (stack.length === 0) {
+        index.push(nums.length);
+      } else {
+        index.push(stack[stack.length - 1].index);
+      }
+    } else {
+      index.push(stack[stack.length - 1].index);
+    }
+    stack.push({ element: nums[i], index: i });
+  }
+  return index.reverse();
+}
+
+function widthArr(nums) {
+  let right = nearestsmllesttoRight(nums);
+  let left = nearestsmllesttoleft(nums);
+  let width = [];
+  for (let i = 0; i < nums.length; i++) {
+    width[i] = right[i] - left[i] - 1;
+  }
+  return width;
+}
+
+function area(nums) {
+  let width = widthArr(nums);
+  let ans = [];
+  max = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    ans[i] = width[i] * nums[i];
+    max = Math.max(ans[i], max);
+  }
+  return max;
+}
+
+const nums = [6, 2, 5, 4, 5, 1, 6];
+console.log(area(nums));
