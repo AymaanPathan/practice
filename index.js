cd7652966b7c1c44c5bdc010dfdc709a0886d480
aymaan <aymaanpathan5@gmail.com>
2025-04-13 19:48:31 +0530

    min stack

--- index.js
+++ index.js
@@ -1,46 +1,27 @@
-function maxOfLeft(nums) {
-  let ans = [];
-  let max = nums[0];
-  for (let i = 0; i < nums.length; i++) {
-    max = Math.max(max, nums[i]);
-    ans[i] = max;
+let stack = [];
+let supportingStack = [];
+function pushStack(a) {
+  stack.push(a);
+  if (
+    supportingStack.length == 0 ||
+    supportingStack[supportingStack.length - 1] >= a
+  ) {
+    supportingStack.push(a);
+    return;
   }
-  return ans;
 }
-
-function maxOfRight(nums) {
-  let ans = [];
-  let max = nums[nums.length - 1];
-  for (let i = nums.length - 1; i >= 0; i--) {
-    max = Math.max(nums[i], max);
-    ans[i] = max;
+function popStack() {
+  if (stack.length === 0) {
+    return -1;
   }
-  return ans;
+  let ans = stack.pop();
+  if (ans === supportingStack[supportingStack.length - 1]) {
+    supportingStack.pop();
+  }
 }
-
-function takeMinimum(nums) {
-  let minimum = [];
-  let right = maxOfRight(nums);
-  let left = maxOfLeft(nums);
-  let min = -Infinity;
-  for (let i = 0; i < nums.length; i++) {
-    min = Math.min(right[i], left[i]);
-    minimum[i] = min;
+function findMin() {
+  if (supportingStack.length === 0) {
+    return -1;
   }
-  return minimum;
+  return supportingStack[supportingStack.length - 1];
 }
-
-var trap = function (height) {
-  let min = takeMinimum(height);
-  let ans = [];
-  for (let i = 0; i < height.length; i++) {
-    ans[i] = min[i] - height[i];
-  }
-  return ans.reduce((acc, sum) => acc + sum, 0);
-};
-
-const nums = [4, 2, 0, 3, 2, 5];
-console.log("ans", trap(nums));
-
-// 1 ans =  we have to do take min(right , left)
-// 2 ans[i] = nums[i]
