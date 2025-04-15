fb2e42e798e0094bcb51a8a52b77ebfa72778101
aymaan <aymaanpathan5@gmail.com>
2025-04-15 23:23:24 +0530

    Remove All Adjacent Duplicates In String

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-var calPoints = function (operations) {
+var removeDuplicates = function (s) {
   let stack = [];
-  for (i = 0; i < operations.length; i++) {
-    if (!isNaN(Number(operations[i]))) {
-      stack.push(Number(operations[i]));
+  let i = 0;
+  while (i < s.length) {
+    if (stack[stack.length - 1] !== s[i]) {
+      stack.push(s[i]);
+      i++;
     } else {
-      if (operations[i] === "C") {
-        stack.pop();
-      } else if (operations[i] === "D") {
-        let ans = 2 * stack[stack.length - 1];
-        stack.push(ans);
-      } else if (operations[i] === "+") {
-        let addprevTwoScores =
-          stack[stack.length - 1] + stack[stack.length - 2];
-
-        stack.push(addprevTwoScores);
-      }
+      stack.pop();
+      i++;
     }
   }
-  return stack.reduce((acc, curr) => acc + curr, 0);
+  return stack.join("");
 };
-const operations = ["5", "2", "C", "D", "+"];
-console.log(calPoints(operations));
+const s = "azxxzy";
+console.log(removeDuplicates(s));
