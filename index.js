539d45ae40460475e14983be80be86b72743f8f8
aymaan <aymaanpathan5@gmail.com>
2025-04-16 22:30:21 +0530

    844. Backspace String Compare

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-var removeDuplicates = function (s) {
-  let stack = [];
+function stack1(s) {
+  let stack1 = [];
   let i = 0;
   while (i < s.length) {
-    if (stack[stack.length - 1] !== s[i]) {
-      stack.push(s[i]);
+    if (s[i] === "#") {
+      stack1.pop();
       i++;
     } else {
-      stack.pop();
+      stack1.push(s[i]);
       i++;
     }
   }
-  return stack.join("");
+  return stack1;
+}
+function stack2(t) {
+  let stack2 = [];
+  let i = 0;
+  while (i < t.length) {
+    if (t[i] === "#") {
+      stack2.pop();
+      i++;
+    } else {
+      stack2.push(t[i]);
+      i++;
+    }
+  }
+  return stack2;
+}
+
+var backspaceCompare = function (s, t) {
+  let stackOne = stack1(s);
+  let stackTwo = stack2(t);
+  if (stackOne.join("") === stackTwo.join("")) {
+    return true;
+  }
+  return false;
 };
-const s = "azxxzy";
-console.log(removeDuplicates(s));
+
+const s = "ab#c";
+const t = "ad#c";
+console.log(backspaceCompare(s, t));
