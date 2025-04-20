b39b7697ace88e8ec7f0c298b699018d1a2ca020
aymaan <aymaanpathan5@gmail.com>
2025-04-20 18:18:54 +0530

    503. Next Greater Element II

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-var asteroidCollision = function (asteroids) {
+var nextGreaterElements = function (nums) {
   let stack = [];
-  for (let i = 0; i < asteroids.length; i++) {
-    let isCurrentDestroyed = false;
-    while (
-      stack.length > 0 &&
-      stack[stack.length - 1] > 0 &&
-      asteroids[i] < 0
-    ) {
-      if (stack[stack.length - 1] < Math.abs(asteroids[i])) {
+  let ans = [];
+  for (let i = 2 * nums.length - 1; i >= 0; i--) {
+    let index = i % nums.length;
+    if (stack.length === 0) {
+      if (i < nums.length) {
+        ans.push(-1);
+      }
+    } else if (stack.length > 0 && stack[stack.length - 1] > nums[index]) {
+      if (i < nums.length) {
+        ans.push(stack[stack.length - 1]);
+      }
+    } else if (stack.length > 0 && stack[stack.length - 1] <= nums[index]) {
+      while (stack.length > 0 && stack[stack.length - 1] <= nums[index]) {
         stack.pop();
-      } else if (stack[stack.length - 1] === Math.abs(asteroids[i])) {
-        stack.pop();
-        isCurrentDestroyed = true;
-        break;
-      } else {
-        isCurrentDestroyed = true;
-        break;
+      }
+      if (i < nums.length) {
+        if (stack.length == 0) {
+          ans.push(-1);
+        } else {
+          ans.push(stack[stack.length - 1]);
+        }
       }
     }
-    if (!isCurrentDestroyed) {
-      stack.push(asteroids[i]);
-    }
+    stack.push(nums[index]);
   }
-  return stack;
+  return ans.reverse();
 };
+const nums = [1, 2, 1];
 
-console.log(asteroidCollision([-5, 10, -15])); // Output: [-5, -15]
-console.log(asteroidCollision([5, 10, -5])); // Output: [5, 10]
-console.log(asteroidCollision([8, -8])); // Output: []
-console.log(asteroidCollision([10, 2, -5])); // Output: [10]
-console.log(asteroidCollision([-2, -1, 1, 2])); // Output: [-2, -1, 1, 2]
+console.log(nextGreaterElements(nums));
