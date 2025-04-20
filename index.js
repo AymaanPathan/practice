05d18da3f6c54d58432d884e0398e2abf56df630
aymaan <aymaanpathan5@gmail.com>
2025-04-20 23:25:50 +0530

    1544. Make The String Great

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
-var nextGreaterElement = function (nums1, nums2) {
+var makeGood = function (s) {
   let stack = [];
-  let ans = [];
-  let map = {};
-  for (let i = nums2.length - 1; i >= 0; i--) {
-    if (stack.length === 0) {
-      if (nums1.includes(nums2[i])) {
-        map[nums2[i]] = -1;
-      }
-    } else if (stack.length > 0 && stack[stack.length - 1] >= nums2[i]) {
-      if (nums1.includes(nums2[i])) {
-        map[nums2[i]] = stack[stack.length - 1];
-      }
-    } else if (stack.length > 0 && stack[stack.length - 1] < nums2[i]) {
-      while (stack.length > 0 && stack[stack.length - 1] < nums2[i]) {
-        stack.pop();
-      }
-      if (stack.length == 0) {
-        map[nums2[i]] = -1;
-      } else if (nums1.includes(nums2[i])) {
-        map[nums2[i]] = stack[stack.length - 1];
-      }
+  let i = 0;
+  while (i < s.length) {
+    if (
+      stack.length > 0 &&
+      stack[stack.length - 1] !== s[i] &&
+      stack[stack.length - 1].toLowerCase() === s[i].toLowerCase()
+    ) {
+      stack.pop();
+    } else {
+      stack.push(s[i]);
     }
-    stack.push(nums2[i]);
+    i++;
   }
-  for (i = 0; i < nums1.length; i++) {
-    ans.push(map[nums1[i]]);
-  }
-  return ans;
+  return stack.join("");
 };
-const nums1 = [4, 1, 2];
-const nums2 = [1, 3, 4, 2];
-console.log(nextGreaterElement(nums1, nums2));
+const s = "leEeetcode";
+console.log(makeGood(s));
