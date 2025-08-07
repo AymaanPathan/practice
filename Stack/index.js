26b5b8fd64f11408d58ed84e39e806951e967b17
aymaan <aymaanpathan5@gmail.com>
2025-08-07 22:37:28 +0530

    add stack q1

--- Stack/index.js
+++ Stack/index.js
@@ -1,45 +1,21 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
+// 1. stack nearest greater to right
+
+function greaterToRight(num) {
+  let stack = [];
+  let ans = [];
+  for (let i = num.length - 1; i >= 0; i--) {
+    while (stack.length > 0 && stack[stack.length - 1] <= num[i]) {
+      stack.pop();
+    }
+    if (stack.length == 0) {
+      ans.push(-1);
+    } else {
+      ans.push(stack[stack.length - 1]);
+    }
+    stack.push(num[i]);
   }
+  return ans.reverse();
 }
 
-class Stack {
-  constructor(val) {
-    let newNode = new Node(val);
-    if (val !== undefined) {
-      this.top = newNode;
-      this.length = 1;
-    } else {
-      this.top = null;
-      this.length = 0;
-    }
-  }
-  Push(val) {
-    //similar to unshift
-    let newNode = new Node(val);
-    if (!this.top || this.length == 0) {
-      this.top = newNode;
-    } else {
-      newNode.next = this.top;
-      this.top = newNode;
-    }
-    this.length++;
-    return this;
-  }
-  pop() {
-    if (this.length == 0 || !this.top) {
-      return undefined;
-    }
-    let temp = this.top;
-    this.top = this.top.next;
-    temp.next = null;
-    this.length--;
-    return temp;
-  }
-}
-const stack = new Stack(1);
-stack.Push(2);
-stack.pop();
-console.log(stack);
+const arr = [1, 3, 2, 4];
+console.log(greaterToRight(arr));
