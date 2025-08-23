103251f9609fd523e2efdd46818c33c2ad7a8558
aymaan <aymaanpathan5@gmail.com>
2025-08-23 11:16:19 +0530

    done baseball game

--- Stack/test.js
+++ Stack/test.js
@@ -1,20 +1,29 @@
-function backSpace(str) {
+// If "+" Record a new score that is the sum of the previous two scores.
+// if "D" Record a new score that is the double of the previous score.
+// if "C" Remove the  stack top
+// Lastly sum it all
+
+var calPoints = function (operations) {
   let stack = [];
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === "#") {
-      console.log("backspace");
+  for (let i = 0; i < operations.length; i++) {
+    if (operations[i] === "+") {
+      let sum = stack[stack.length - 1] + stack[stack.length - 2];
+      stack.push(sum);
+    } else if (operations[i] === "D") {
+      let double = stack[stack.length - 1] * 2;
+      stack.push(double);
+    } else if (operations[i] === "C") {
       stack.pop();
     } else {
-      stack.push(str[i]);
+      stack.push(Number(operations[i]));
     }
   }
-  return stack.join("");
-}
-
-var backspaceCompare = function (s, t) {
-  let str1BackSpace = backSpace(s);
-  let str2BackSpace = backSpace(t);
-  return str1BackSpace===str2BackSpace
+  let ans = 0;
+  for (let i = 0; i < stack.length; i++) {
+    ans += stack[i];
+  }
+  return ans;
 };
-const s = "ab#c";
-console.log(backSpace(s));
+
+const ops = ["5", "2", "C", "D", "+"];
+console.log(calPoints(ops));
